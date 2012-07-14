55f996d496c7ce3985bd5763341ad3fdbc2b0d25
Rene Pickhardt <rene@rene-pickhardt.de>
2012-07-14 10:53:46 +0200

    created a config file to store global config variables

---
+++ src/de/typology/utils/Config.java
@@ -0,0 +1,97 @@
+package de.typology.utils;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.Properties;
+
+/**
+ * This is an interface class to the Config file for this project. For each
+ * class field one java property must be defined in config.txt. The fields will
+ * be automatically filled! 
+ * 
+ * Allowed Types are String, int, boolean, String[] and long[]
+ * where arrays are defined by semicolon-separated Strings like "array=a;b;c"
+ * boolen fields are initialized with true or false
+ * 
+ * lines starting with # will be ignored and can serve as comments
+ * 
+ * @author Jonas Kunze, Rene Pickhardt
+ * 
+ */
+public class Config extends Properties {
+	public String wikiXmlPath; 
+	public String parsedWikiOutputPath;
+	
+	private static final long serialVersionUID = -4439565094382127683L;
+
+	static Config instance = null;
+
+	public Config() {
+		String file = "config.txt";
+		try {
+			BufferedInputStream stream = new BufferedInputStream(
+					new FileInputStream(file));
+			load(stream);
+			stream.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		try {
+			initialize();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Fills all fields with the data defined in the config file.
+	 * 
+	 * @throws IllegalArgumentException
+	 * @throws IllegalAccessException
+	 */
+	private void initialize() throws IllegalArgumentException,
+			IllegalAccessException {
+		Field[] fields = this.getClass().getFields();
+		for (Field f : fields) {
+			if (getProperty(f.getName()) == null) {
+				System.err.print("Property '" + f.getName()
+						+ "' not defined in config file");
+			}
+			if (f.getType().equals(String.class)) {
+				f.set(this, getProperty(f.getName()));
+			} else if (f.getType().equals(long.class)) {
+				f.setLong(this, Long.valueOf(getProperty(f.getName())));
+			} else if (f.getType().equals(int.class)) {
+				f.setInt(this, Integer.valueOf(getProperty(f.getName())));
+			} else if (f.getType().equals(boolean.class)) {
+				f.setBoolean(this, Boolean.valueOf(getProperty(f.getName())));
+			} else if (f.getType().equals(String[].class)) {
+				f.set(this, getProperty(f.getName()).split(";"));
+			} else if (f.getType().equals(int[].class)) {
+				String[] tmp = getProperty(f.getName()).split(";");
+				int[] ints = new int[tmp.length];
+				for (int i = 0; i < tmp.length; i++) {
+					ints[i] = Integer.parseInt(tmp[i]);
+				}
+				f.set(this, ints);
+			} else if (f.getType().equals(long[].class)) {
+				String[] tmp = getProperty(f.getName()).split(";");
+				long[] longs = new long[tmp.length];
+				for (int i = 0; i < tmp.length; i++) {
+					longs[i] = Long.parseLong(tmp[i]);
+				}
+				f.set(this, longs);
+			}
+		}
+	}
+
+	public static Config get() {
+		if (instance == null)
+			instance = new Config();
+		return instance;
+	}
+}
