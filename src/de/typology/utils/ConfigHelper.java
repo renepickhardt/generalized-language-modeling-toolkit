49f12427bd304d30f6a76e57b117e0804882b437
Rene Pickhardt <rene@rene-pickhardt.de>
2012-08-16 12:19:35 +0200

    =fast typology retrieval

---
+++ src/de/typology/utils/ConfigHelper.java
@@ -0,0 +1,79 @@
+/*
+ * Helper to handle constants and config files
+ *
+ * All values are global static values used in complet
+ * To use config file instead of standard values call loadConfigFile()
+ *
+ * Note that PATH variables can be overwritten by calling classes!
+ *
+ * @author Paul Wagner
+ *
+ */
+package de.typology.utils;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class ConfigHelper {
+
+	// MAIN CONFIG
+	public static String NAME_KEY = "word";
+	public static String COUNT_KEY = "cnt";
+	public static String RELLOC_KEY = "rel_loc";
+	public static String RELSUM_KEY = "rel_sum";
+	public static int MAX_RELATIONS = 4;
+
+	public static int RELATIONSHIPSTORE_MEM = 100;
+	public static int PROPERTYSTORE_MEM = 150;
+	public static String CACHE_TYPE = "strong";
+	public static Boolean BUFFER = true;
+
+	public static String IN_PATH = "";
+	public static String OUT_PATH = "";
+	public static String DB_PATH = "";
+
+	// SERVER CONFIG
+	public static int SRV_PORT = 1337;
+	public static int P1 = 5;
+	public static int P2 = 2;
+	public static int P3 = 2;
+	public static int P4 = 0;
+
+	public static void loadConfigFile(String config_file) {
+		if (!config_file.isEmpty()) {
+			try {
+				Properties p = new Properties();
+				p.load(new FileInputStream(config_file));
+				IN_PATH = p.getProperty("IN_PATH", IN_PATH);
+				OUT_PATH = p.getProperty("OUT_PATH", OUT_PATH);
+				DB_PATH = p.getProperty("DB_PATH", DB_PATH);
+				NAME_KEY = p.getProperty("NAME_KEY", NAME_KEY);
+				COUNT_KEY = p.getProperty("COUNT_KEY", COUNT_KEY);
+				RELLOC_KEY = p.getProperty("RELLOC_KEY", RELLOC_KEY);
+				RELSUM_KEY = p.getProperty("RELLOC_KEY", RELSUM_KEY);
+				SRV_PORT = Integer.parseInt(p.getProperty("SRV_PORT", SRV_PORT
+						+ ""));
+				P1 = Integer.parseInt(p.getProperty("P1", P1 + ""));
+				P2 = Integer.parseInt(p.getProperty("P2", P2 + ""));
+				P3 = Integer.parseInt(p.getProperty("P3", P3 + ""));
+				P4 = Integer.parseInt(p.getProperty("P4", P4 + ""));
+				MAX_RELATIONS = Integer.parseInt(p.getProperty("MAX_RELATIONS",
+						MAX_RELATIONS + ""));
+				RELATIONSHIPSTORE_MEM = Integer.parseInt(p.getProperty(
+						"RELATIONSHIPSTORE_MEM", RELATIONSHIPSTORE_MEM + ""));
+				PROPERTYSTORE_MEM = Integer.parseInt(p.getProperty(
+						"PROPERTYSTORE_MEM", PROPERTYSTORE_MEM + ""));
+				CACHE_TYPE = p.getProperty("CACHE_TYPE", CACHE_TYPE);
+				if (p.getProperty("BUFFER", "TRUE").toUpperCase()
+						.equals("TRUE")) {
+					BUFFER = true;
+				} else {
+					BUFFER = false;
+				}
+			} catch (Exception e) {
+				IOHelper.logError("(ConfigHelper) Error parsing config file...");
+			}
+		}
+
+	}
+}
---
+++ src/de/typology/utils/IOHelper.java
@@ -0,0 +1,191 @@
+/*
+ * Helper class for input and output
+ *
+ * @author René Pickhardt
+ *
+ */
+package de.typology.utils;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Date;
+
+public class IOHelper {
+	// d = debug mode set true if debugg messages should be displayed
+	private static boolean d = true;
+	private static BufferedWriter logFile = openAppendFile("Complet.log");
+	private static BufferedWriter strongLogFile = openAppendFile("Complet.strong.log");
+
+	/**
+	 * faster access to a buffered reader
+	 * 
+	 * @param filename
+	 * @return buffered reader for file input
+	 */
+	public static BufferedReader openReadFile(String filename) {
+		FileInputStream fstream;
+		BufferedReader br = null;
+		try {
+			fstream = new FileInputStream(filename);
+			DataInputStream in = new DataInputStream(fstream);
+			br = new BufferedReader(new InputStreamReader(in));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+		return br;
+	}
+
+	/**
+	 * Faster access to a bufferedWriter
+	 * 
+	 * @param filename
+	 * @return buffered writer which can be used for output
+	 */
+	public static BufferedWriter openWriteFile(String filename) {
+		FileWriter filestream = null;
+		try {
+			filestream = new FileWriter(filename);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+		return new BufferedWriter(filestream);
+	}
+
+	/**
+	 * Faster access to a bufferedWriter that appands to a fil
+	 * 
+	 * @param filename
+	 * @return buffered writer which can be used for output
+	 */
+	public static BufferedWriter openAppendFile(String filename) {
+		FileWriter filestream = null;
+		try {
+			filestream = new FileWriter(filename, true);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+		return new BufferedWriter(filestream);
+	}
+
+	/**
+	 * function for output that is only displayed in debugmode
+	 * 
+	 * @param out
+	 */
+	public static void pln(Object out) {
+		if (d) {
+			System.out.println(out);
+		}
+	}
+
+	/**
+	 * function for output that is only displayed in debugmode
+	 * 
+	 * @param out
+	 */
+	public static void p(Object out) {
+		if (d) {
+			System.out.print(out);
+		}
+	}
+
+	/*
+	 * function for error-output that is only displayed in debugmode
+	 * 
+	 * @param out
+	 */
+	public static void perr(Object out) {
+		if (d) {
+			System.err.println(out);
+		}
+	}
+
+	/**
+	 * function for output that is only displayed in debugmode
+	 * 
+	 * @param out
+	 */
+	public static void log(Object out) {
+		if (d) {
+			System.out.println(out);
+		}
+		try {
+			Date dt = new Date();
+			logFile.write(dt + " - " + out + "\n");
+			logFile.flush();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * function for output that is only displayed in debugmode
+	 * 
+	 * @param out
+	 */
+	public static void logError(Object out) {
+		if (d) {
+			System.err.println(out);
+		}
+		try {
+			Date dt = new Date();
+			logFile.write(dt + " - ERROR:" + out + "\n");
+			logFile.flush();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * function for output that is only displayed in debugmode
+	 * 
+	 * @param out
+	 */
+	public static void strongLog(Object out) {
+		log("!!!!!" + out);
+		try {
+			Date dt = new Date();
+			strongLogFile.write(dt + " - " + out + "\n");
+			strongLogFile.flush();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static boolean deleteDirectory(String path) {
+		return deleteDirectory(new File(path));
+	}
+
+	/**
+	 * @param path
+	 *            : filepath to the directory that needs to be deleted
+	 * @return true if successful
+	 */
+	public static boolean deleteDirectory(File path) {
+		if (path.exists()) {
+			File[] files = path.listFiles();
+			for (File file : files) {
+				if (file.isDirectory()) {
+					deleteDirectory(file);
+				} else {
+					file.delete();
+				}
+			}
+		}
+		return path.delete();
+	}
+}
