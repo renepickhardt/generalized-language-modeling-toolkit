b3290a9e31ed807a1809a4b6fbfb29fe1abebb2d
Rene Pickhardt <rene@rene-pickhardt.de>
2012-08-21 16:31:10 +0200

    added linux sorting to algorithm need to touch config files though

--- src/de/typology/nGramBuilder/BuildNGrams.java
+++ src/de/typology/nGramBuilder/BuildNGrams.java
@@ -3,6 +3,7 @@ package de.typology.nGramBuilder;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 import de.typology.utils.Config;
 import de.typology.utils.IOHelper;
@@ -17,8 +18,10 @@ public class BuildNGrams {
 				.openReadFile(Config.get().parsedWikiOutputPath);
 		BufferedWriter bw = IOHelper.openWriteFile(Config.get().parsedNGrams);
 		String line = "";
+		int cnt = 0;
 		try {
 			while ((line = br.readLine()) != null) {
+				cnt++;
 				String[] tokens = line.split(" ");
 				for (int i = Config.get().nGramLength; i < tokens.length; i++) {
 					for (int j = i - Config.get().nGramLength; j < i; j++) {
@@ -30,9 +33,24 @@ public class BuildNGrams {
 					bw.write("\n");
 				}
 				bw.flush();
+				if (cnt > 10) {
+					break;
+				}
 			}
 			bw.close();
 			br.close();
+
+			Process p = Runtime
+					.getRuntime()
+					.exec("sort --output=/var/lib/datasets/test /var/lib/datasets/parsedNGrams.txt");
+			BufferedReader stdInput = new BufferedReader(new InputStreamReader(
+					p.getInputStream()));
+			String s = "";
+			while ((s = stdInput.readLine()) != null) {
+				System.out.println(s);
+			}
+			System.out.println("sorting done need to aggregate now");
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
