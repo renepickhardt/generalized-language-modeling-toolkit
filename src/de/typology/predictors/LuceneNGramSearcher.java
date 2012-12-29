bc18c9780d1af02a01ad9315c62cc75bcf94cebe
koerner <mkoerner@uni-koblenz.de>
2012-12-29 17:14:49 +0100

    updated LuceneNGramSearcher (work in progress)

--- src/de/typology/predictors/LuceneNGramSearcher.java
+++ src/de/typology/predictors/LuceneNGramSearcher.java
@@ -1,5 +1,6 @@
 package de.typology.predictors;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -21,6 +22,7 @@ import org.apache.lucene.search.SortField;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.store.MMapDirectory;
 import org.apache.lucene.util.Version;
 
 import de.typology.utils.Algo;
@@ -30,19 +32,47 @@ import de.typology.utils.IOHelper;
 public class LuceneNGramSearcher {
 
 	private ArrayList<IndexSearcher> index;
+	private Sort sort;
+	private QueryParser queryParser;
+	private FieldValueFilter fieldValueFilter;
+	private float[][] weights;
 
+	// done:<
 	public LuceneNGramSearcher() {
 		this.index = new ArrayList<IndexSearcher>();
 		try {
 			for (int i = 2; i < 6; i++) {
+				// i=ngram index type
 				Directory directory;
 				DirectoryReader directoryReader;
-				directory = FSDirectory.open(new File(
-						Config.get().nGramIndexPath + i + "/"));
+				if (Config.get().loadIndexToRAM) {
+					directory = MMapDirectory.open(new File(
+							Config.get().nGramIndexPath + i + "/"));
+				} else {
+					directory = FSDirectory.open(new File(
+							Config.get().nGramIndexPath + i + "/"));
+				}
 				directoryReader = DirectoryReader.open(directory);
 				this.index.add(new IndexSearcher(directoryReader));
 
 			}
+			// >
+			// copy&paste:<
+			SortField sortField = new SortField("cnt", SortField.Type.FLOAT,
+					true);
+			this.sort = new Sort(sortField);
+			Analyzer analyzer = new KeywordAnalyzer();
+			this.queryParser = new QueryParser(Version.LUCENE_40, "src",
+					analyzer);
+			this.queryParser.setLowercaseExpandedTerms(false);
+			this.fieldValueFilter = new FieldValueFilter("cnt");
+			this.weights = new float[100][Config.get().nGramLength];
+			for (int i = 0; i < 100; i++) {
+				for (int j = 0; j < Config.get().nGramLength; j++) {
+					this.weights[i][j] = 0;
+				}
+			}
+			// >
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -55,12 +85,18 @@ public class LuceneNGramSearcher {
 
 	}
 
-	public void query(String q, String prefix, String match) {
-		IOHelper.log(q + " \tPREFIX: " + prefix + " \tMATCH: " + match);
-		HashMap<String, Float> result = this.search(q, prefix, 12);
+	// done (except comments)<
+	public int query(String q, String prefix, String match,
+			int intermediateListLength, int k) {
+
+		// IOHelper.logLearn("TYPOLOGY - QUERY: " + q + " PREFIXLENGTH: "
+		// + prefix.length() + " MATCH: " + match);
+
+		HashMap<String, Float> result = this.search(q, prefix,
+				intermediateListLength, match);
 		Algo<String, Float> a = new Algo<String, Float>();
 		TreeMap<Float, Set<String>> topkSuggestions = a.getTopkElements(result,
-				5);
+				k);
 		int topkCnt = 0;
 
 		for (Float score : topkSuggestions.descendingKeySet()) {
@@ -72,59 +108,70 @@ public class LuceneNGramSearcher {
 					if (topkCnt == 1) {
 						IOHelper.log("KSS: "
 								+ (match.length() - prefix.length())
-								+ " \tPREFIXLENGHT: " + prefix.length());
+						// + " \tPREFIXLENGHT: " + prefix.length() was removed
+						// in TypologySearcher
+						);
 					}
-					return;
+					return topkCnt;
 				}
 			}
 		}
 		IOHelper.log("NOTHING\tPREFIXLENGTH: " + prefix.length());
-	}
+		return -1;
+	}// >
+
+	// not sure:<
 
 	// docs at:
 	// http://www.ibm.com/developerworks/library/os-apache-lucenesearch/
 	// http://stackoverflow.com/questions/468405/how-to-incorporate-multiple-fields-in-queryparser
 	// http://oak.cs.ucla.edu/cs144/projects/lucene/index.html in chapter 2
 	public HashMap<String, Float> search(String q, String prefix,
-			int numIntermediateLists) {
+			int numIntermediateLists, String match) {
 		HashMap<String, Float> result = new HashMap<String, Float>();
 		try {
-			SortField sortField = new SortField("cnt", SortField.Type.FLOAT,
-					true);
-			Sort sort = new Sort(sortField);
-
-			Analyzer analyzer = new KeywordAnalyzer();
-			QueryParser queryParser = new QueryParser(Version.LUCENE_40, "src",
-					analyzer);
-
-			queryParser.setLowercaseExpandedTerms(false);
 
 			String[] terms = this.prepareQuery(q);
+			// extend prepareQuery?
 			if (terms == null) {
 				return null;
 			}
+			int ngram = 0;
 
-			ArrayList<TopDocs> hits = new ArrayList<TopDocs>();
+			// ArrayList<TopDocs> hits = new ArrayList<TopDocs>();
 
 			for (int i = 0; i < terms.length; i++) {
-				String special = "src:\"" + terms[i] + "\"";
+				String special = "src:" + terms[i];
 				if (prefix.length() > 0) {
-					special = "src:\"" + terms[i] + "\" AND tgt:" + prefix
-							+ "*";
+					special = special.concat(" AND tgt:" + prefix + "*");
 				}
 
-				TopDocs results = this.index.get(i)
-						.search(queryParser.parse(special),
-								new FieldValueFilter("cnt"),
-								numIntermediateLists, sort);
+				TopDocs results = this.index.get(i).search(
+						this.queryParser.parse(special), this.fieldValueFilter,
+						numIntermediateLists, this.sort);
 
-				hits.add(results);
-
+				// hits.add(results);
+				int rank = 1;
 				for (ScoreDoc scoreDoc : results.scoreDocs) {
 					Document doc = this.index.get(i).doc(scoreDoc.doc);
 
 					String key = doc.get("tgt");
 					Float value = Float.parseFloat(doc.get("cnt"));
+
+					if (key.equals(match)) {
+						this.weights[prefix.length()][ngram + 1] += 1 / (float) rank;
+					}
+					rank++;
+					// String res = "";
+					// if (key.equals(match)) {
+					// res = " \tHIT";
+					// } else {
+					// res = " \tNOMATCH";
+					// }
+					// IOHelper.logLearn("FROM: " + terms[i] + " \tEDGETYPE: "
+					// + (edge + 1) + " \t RANK: " + rank++
+					// + " \tPREDICTS: " + key + "\t SCORE: " + value
+					// + res);
 
 					if (result.containsKey(key)) {
 						result.put(key, value + result.get(key));
@@ -132,6 +179,7 @@ public class LuceneNGramSearcher {
 						result.put(key, value);
 					}
 				}
+				ngram++;
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -141,6 +189,8 @@ public class LuceneNGramSearcher {
 		}
 		return result;
 	}
+
+	// >
 
 	// query format at least
 	// input: w0 w1 w2 w3
@@ -175,4 +225,31 @@ public class LuceneNGramSearcher {
 		}
 		return null;
 	}
+
+	// copy&paste:<
+	public void saveWeights(int numQueries) {
+		BufferedWriter bw = IOHelper.openWriteFile("weights");
+		try {
+			for (int i = 0; i < 10; i++) {
+				bw.write("PREFIXLENGTH: " + i);
+				float max = 0;
+				for (int j = 1; j < Config.get().nGramLength; j++) {
+					if (this.weights[i][j] > max) {
+						max = this.weights[i][j];
+					}
+				}
+				// TODO: can be divided by numQueries
+				for (int j = 1; j < Config.get().nGramLength; j++) {
+					bw.write(" w" + j + ": " + (int) this.weights[i][j] * 1000
+							/ max);
+				}
+				bw.write("\n");
+			}
+			bw.flush();
+			bw.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}// >
 }
