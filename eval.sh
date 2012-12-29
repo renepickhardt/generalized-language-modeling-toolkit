7fce930a776b759e051e96b25cce4e1f36541882
Rene Pickhardt <rene@rene-pickhardt.de>
2012-12-29 15:33:31 +0800

    new evaluation shell scripts

--- eval.sh
+++ eval.sh
@@ -1,11 +1,21 @@
 #AUTOR: Rene Pickhardt
 #Purpose to aggregate data from logfiles
 #run with ./eval.sh logFileName
-
 LOGFILE=$1
+
+#prefix length
+PFL=5
+#precision at k up to PAK
+PAK=6
+#maximal number of ranks that ar included in MRR every result ranking worse is set to 0
+MRRMAXRANK=10
+
+for LOGFILE in "$@"
+do
 NUMQUERIES=`grep "MATCH:" $LOGFILE | wc -l`
 
 #### calculate and output average key stroke savings
+echo "Calculate and output the average key stroke savings"
 SUM=0
 for((  i = 1 ;  i <= 20;  i++  ))
 do
@@ -15,31 +25,32 @@ MUL=$((KSS * i))
 SUM=$((SUM + MUL))
 done
 AVGKSS=`echo "$SUM / $NUMQUERIES " | bc -l`
-echo "Average Key stroke savings: $AVGKSS "
+echo "Average Key stroke savings: $AVGKSS " | tee "res.$LOGFILE"
 
 #### Precision @ k for every prefix length
-for((  i = 0 ;  i <= 10;  i++  ))
+echo "precision @ k for every prefix with PFL = $PFL and k up to: $PAK"
+for((  i = 0 ;  i <= PFL;  i++  ))
 do
 NUMQUERIES=`grep "PREFIXLENGTH: $i" $LOGFILE | wc -l`
 echo "$NUMQUERIES queries of with prefix of length $i"
 HIT=0
-for((  j = 1 ;  j <= 10;  j++  ))
+for((  j = 1 ;  j <= PAK;  j++  ))
 do
 RES=`grep "PREFIXLENGTH: $i" $LOGFILE | grep "RANK: $j" | wc -l`
 HIT=$((RES + HIT))
 precisionAtK=`echo "$HIT / $NUMQUERIES " | bc -l`
-echo "   Precision at k= $j: $precisionAtK"
+echo "   Precision at k = $j: $precisionAtK PFL = $i" | tee -a "res.$LOGFILE"
 done
 done
 
 #### MRR
-
-for((  i = 0 ;  i <= 10;  i++  ))
+echo "TESTS for MRR with PFL = $PFL and RANKS up to $MRRMAXRANK"
+for((  i = 0 ;  i <= PFL;  i++  ))
 do
 NUMQUERIES=`grep "PREFIXLENGTH: $i" $LOGFILE | wc -l`
-echo "$NUMQUERIES queries of with prefix of length $i"
+#echo "$NUMQUERIES queries of with prefix of length $i"
 HIT=0
-for((  j = 1 ;  j <= 10;  j++  ))
+for((  j = 1 ;  j <= MRRMAXRANK;  j++  ))
 do
 CNT=`grep "PREFIXLENGTH: $i" $LOGFILE | grep "RANK: $j" | wc -l`
 WRR=`echo "$CNT / $j " | bc -l`
@@ -47,5 +58,8 @@ HIT=`echo "$HIT + $WRR " | bc -l`
 #HIT=$((HIT + WRR))
 done
 MRR=`echo "$HIT / $NUMQUERIES " | bc -l`
-echo "   MRR = $MRR:"
+echo "   MRR = $MRR: PFL = $i" | tee -a "res.$LOGFILE"
 done
+
+done
+
--- src/de/typology/evaluation/TypologyEvaluator.java
+++ src/de/typology/evaluation/TypologyEvaluator.java
@@ -11,69 +11,84 @@ public class TypologyEvaluator {
 	public static void main(String[] args) {
 		LuceneTypologySearcher lts = new LuceneTypologySearcher();
 
-		for (int n = 4; n < 6; n++) {
+		// int joinLength = 12;
+		int topK = 10;
 
-			BufferedReader br = IOHelper.openReadFile(Config.get().testingPath);
-			try {
-				String line = "";
-				long start = System.currentTimeMillis();
-				int cnt = 0;
-				IOHelper.setResultFile("typo-" + n + "-"
-						+ Config.get().sampleRate + Config.get().splitDataRatio
-						+ ".log." + start);
-				IOHelper.log("!!!!!!!!!!TYPOLOGY EVAL: N = " + n);
-				while ((line = br.readLine()) != null) {
-					String sentence = line;
-					String[] words = sentence.split("\\ ");
-					if (words.length < n) {
-						continue;
-					}
-					boolean flag = false;
-					for (int l = 0; l < n; l++) {
-						if (words[l].length() < 1) {
-							flag = true;
+		for (int joinLength = 5; joinLength < 50; joinLength = joinLength + 2) {
+			for (int n = 2; n < 6; n++) {
+
+				BufferedReader br = IOHelper
+						.openReadFile(Config.get().testingPath);
+				try {
+					String line = "";
+					long start = System.currentTimeMillis();
+					int cnt = 0;
+					int queries = 0;
+					IOHelper.setResultFile("joinlog/typo-" + n + "-joinLengh-"
+							+ joinLength + "-" + Config.get().sampleRate
+							+ Config.get().splitDataRatio + ".log." + start);
+					IOHelper.log("!!!!!!!!!!TYPOLOGY EVAL: N = " + n);
+					while ((line = br.readLine()) != null) {
+						String sentence = line;
+						String[] words = sentence.split("\\ ");
+						if (words.length < n) {
+							continue;
 						}
-					}
-					if (flag) {
-						continue;
-					}
+						boolean flag = false;
+						for (int l = 0; l < n; l++) {
+							if (words[l].length() < 1) {
+								flag = true;
+							}
+						}
+						if (flag) {
+							continue;
+						}
 
-					String query = "";
-					if (n == 5) {
-						query = words[0] + " " + words[1] + " " + words[2]
-								+ " " + words[3];
-					} else if (n == 4) {
-						query = words[0] + " " + words[1] + " " + words[2];
-					} else if (n == 3) {
-						query = words[0] + " " + words[1];
-					} else if (n == 2) {
-						query = words[0];
-					}
+						String query = "";
+						if (n == 5) {
+							query = words[0] + " " + words[1] + " " + words[2]
+									+ " " + words[3];
+						} else if (n == 4) {
+							query = words[0] + " " + words[1] + " " + words[2];
+						} else if (n == 3) {
+							query = words[0] + " " + words[1];
+						} else if (n == 2) {
+							query = words[0];
+						}
 
-					// for (int j = 0; j < Math.min(words[n - 1].length(), 4);
-					// j++) {
-					IOHelper.logResult(query + "  \tMATCH: " + words[n - 1]);
-					for (int j = 0; j < words[n - 1].length() - 1; j++) {
-						// include more logic
-						int res = lts.query(query,
-								words[n - 1].substring(0, j), words[n - 1], 12,
-								10);
-						cnt++;
-						if (cnt % 250 == 0) {
+						// for (int j = 0; j < Math.min(words[n - 1].length(),
+						// 4);
+						// j++) {
+						IOHelper.logResult(query + "  \tMATCH: " + words[n - 1]);
+						for (int j = 0; j < words[n - 1].length() - 1; j++) {
+							// include more logic
+							int res = lts.query(query,
+									words[n - 1].substring(0, j), words[n - 1],
+									joinLength, topK);
+							cnt++;
+							if (cnt % 2000 == 0) {
+								long time = System.currentTimeMillis() - start;
+								IOHelper.strongLog(cnt + " predictions in "
+										+ time + " ms \t" + cnt * 1000 / time
+										+ " predictions / sec");
+							}
+							if (res == 1) {
+								break;
+							}
+						}
+						queries++;
+						if (queries % 250 == 0) {
 							long time = System.currentTimeMillis() - start;
-							IOHelper.strongLog(cnt + " predictions in " + time
-									+ " ms \t" + cnt * 1000 / time
-									+ " predictions / sec");
-						}
-						if (res == 1) {
-							break;
+							IOHelper.strongLog(queries + " queries in " + time
+									+ " ms \t" + queries * 1000 / time
+									+ " queries / sec");
 						}
 					}
+					// }
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
 				}
-				// }
-				br.close();
-			} catch (IOException e) {
-				e.printStackTrace();
 			}
 		}
 	}
