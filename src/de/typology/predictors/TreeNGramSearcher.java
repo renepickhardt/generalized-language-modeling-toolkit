0fff04c72bf1a09480628d7bec4e64349903d17f
Rene Pickhardt <rene@rene-pickhardt.de>
2013-01-04 15:44:52 +0800

    make code syntactically correct

--- src/de/typology/predictors/TreeNGramSearcher.java
+++ src/de/typology/predictors/TreeNGramSearcher.java
@@ -11,30 +11,26 @@ import de.typology.trainers.SuggestTree.Pair;
 import de.typology.trainers.TreeIndexer;
 import de.typology.utils.Config;
 
-public class TreeNGramSearcher extends TreeSearcher{
-	public TreeNGramSearcher(int n, int k, int joinLength,HashMap <Integer,HashMap<String,SuggestTree<Float>>> treeMapMap) {
+public class TreeNGramSearcher extends TreeSearcher {
+	public TreeNGramSearcher(int n, int k, int joinLength) {
 		super(n, k, joinLength);
-		TreeNGramSearcher.treeMapMap=treeMapMap;
-
 	}
 
-	protected static HashMap <Integer,HashMap<String,SuggestTree<Float>>> treeMapMap;
 	/**
 	 * @param args
-	 * @throws IOException 
+	 * @throws IOException
 	 */
 	public static void main(String[] args) throws IOException {
-		TreeIndexer tti=new TreeIndexer();
-		HashMap <Integer,HashMap<String,SuggestTree<Float>>> treeMapMap= tti.run(Config.get().normalizedNGrams);
-		TreeNGramSearcher tns=new TreeNGramSearcher(5, 5, 12,treeMapMap);
+		TreeIndexer tti = new TreeIndexer();
+		tti.run(Config.get().normalizedNGrams);
+		TreeNGramSearcher tns = new TreeNGramSearcher(5, 5, 12);
 		tns.query("1991 1992 auf 1994", "a", "aus");
-		HashMap<String, Float>result=tns.search("1991 1992 auf 1994", "a","");
-		for(Entry<String, Float> e:result.entrySet()){
-			System.out.println(e.getKey()+" "+e.getValue());
+		HashMap<String, Float> result = tns.search("1991 1992 auf 1994", "a",
+				"");
+		for (Entry<String, Float> e : result.entrySet()) {
+			System.out.println(e.getKey() + " " + e.getValue());
 		}
 	}
-
-
 
 	@Override
 	public HashMap<String, Float> search(String q, String prefix, String match) {
@@ -46,35 +42,41 @@ public class TreeNGramSearcher extends TreeSearcher{
 		}
 
 		for (int i = 0; i < terms.length; i++) {
-			trees=new ArrayList<SuggestTree<Float>>();
-			//get list of trees
-			String t1=terms[0].substring(0, 1);
-			String t2=terms[1].substring(0, 1);
-			if(prefix.length()!=0){
-				if(treeMapMap.get(i+2).containsKey(t1)){
-					trees.add(treeMapMap.get(i+2).get(t1));
-				}else{
-					if(treeMapMap.get(i+2).containsKey(t1+t2)){
-						trees.add(treeMapMap.get(i+2).get(t1+t2));
-					}else{
-						if(treeMapMap.get(i+2).containsKey(t1+"other")){
-							trees.add(treeMapMap.get(i+2).get(t1+"other"));
-						}else{
-							trees.add(treeMapMap.get(i+2).get("other"));
+			trees = new ArrayList<SuggestTree<Float>>();
+			// get list of trees
+			String t1 = terms[0].substring(0, 1);
+			String t2 = terms[1].substring(0, 1);
+			if (prefix.length() != 0) {
+				if (TreeIndexer.treeMapMap.get(i + 2).containsKey(t1)) {
+					trees.add(TreeIndexer.treeMapMap.get(i + 2).get(t1));
+				} else {
+					if (TreeIndexer.treeMapMap.get(i + 2).containsKey(t1 + t2)) {
+						trees.add(TreeIndexer.treeMapMap.get(i + 2)
+								.get(t1 + t2));
+					} else {
+						if (TreeIndexer.treeMapMap.get(i + 2).containsKey(
+								t1 + "other")) {
+							trees.add(TreeIndexer.treeMapMap.get(i + 2).get(
+									t1 + "other"));
+						} else {
+							trees.add(TreeIndexer.treeMapMap.get(i + 2).get(
+									"other"));
 						}
 					}
 				}
 			}
 
-			float weight=1;
-			for(SuggestTree<Float> tree:trees){
-				Node<Float> node=tree.getBestSuggestions(terms[i]+" "+prefix);
-				if(node!=null){
-					for(int join=0;join<this.joinLength&&join<node.listLength();join++){
-						Pair<Float> pair=node.getSuggestion(join);
-						String[] split=pair.getString().split(" ");
-						String key=split[split.length-1];
-						Float value=pair.getScore();
+			float weight = 1;
+			for (SuggestTree<Float> tree : trees) {
+				Node<Float> node = tree.getBestSuggestions(terms[i] + " "
+						+ prefix);
+				if (node != null) {
+					for (int join = 0; join < this.joinLength
+							&& join < node.listLength(); join++) {
+						Pair<Float> pair = node.getSuggestion(join);
+						String[] split = pair.getString().split(" ");
+						String key = split[split.length - 1];
+						Float value = pair.getScore();
 						if (result.containsKey(key)) {
 							result.put(key, weight * value + result.get(key));
 						} else {
@@ -120,21 +122,17 @@ public class TreeNGramSearcher extends TreeSearcher{
 		return null;
 	}
 
-
 	@Override
 	public String getFileName() {
 		String name = "";
 		if (Config.get().useWeights) {
 			name = name.concat("weighted-");
 		}
-		name = name.concat("typo-" + this.n+ "-joinLengh-" + this.joinLength
+		name = name.concat("typo-" + this.n + "-joinLengh-" + this.joinLength
 				+ "-" + Config.get().sampleRate + Config.get().splitDataRatio
 				+ ".log");
 		// TODO Auto-generated method stub
 		return name;
 	}
-
-
-
 
 }
