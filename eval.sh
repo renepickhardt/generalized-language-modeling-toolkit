567e36fabaa459fda82e399950e3eccfd7686782
koerner <mkoerner@uni-koblenz.de>
2013-01-17 21:13:31 +0100

    updated eval.sh

--- eval.sh
+++ eval.sh
@@ -7,7 +7,7 @@ LOGFILE=$1
 #prefix length
 PFL=5
 #precision at k up to PAK
-PAK=6
+PAK=5
 #maximal number of ranks that ar included in MRR every result ranking worse is set to 0
 MRRMAXRANK=10
 
@@ -18,15 +18,51 @@ NUMQUERIES=`grep "MATCH:" $LOGFILE | wc -l`
 #### calculate and output average key stroke savings
 echo "Calculate and output the average key stroke savings"
 SUM=0
-for((  i = 1 ;  i <= 20;  i++  ))
+
+
+#change this if PAK!=5
+PARAMETERS=(0 0 0 0 0)
+RESULTS=(0 0 0 0 0)
+#read lines and store values into PARAMETERS
+(while read -r line
+do 
+#store results into RESULTS
+if [[ "$line" == *"MATCH"* ]]
+then
+for((  atk = 1 ;  atk <= PAK;  atk++  ))
 do
-KSS=`grep "KSS: $i" $LOGFILE | wc -l`
-echo "$i keystrokes saved $KSS times"
-MUL=$((KSS * i))
-SUM=$((SUM + MUL))
+RESULT[${atk-1}]=$((RESULT[${atk-1}]+PARAMETERS[${atk-1}]))
+echo "RESULT[${atk-1}]:${RESULT[${atk-1}]}"
 done
-AVGKSS=`echo "$SUM / $NUMQUERIES " | bc -l`
-echo "Average Key stroke savings: $AVGKSS " | tee "res.$LOGFILE"
+PARAMETERS=(0 0 0 0 0 0)
+fi
+
+for((  atk = 1 ;  atk <= PAK;  atk++  ))
+do
+if [[ "$line" == "KSS AT $atk: "* ]]
+then
+	PARAMETERS[${atk-1}]=${line##*:}
+	echo "PARAMETERS[${atk-1}]:${PARAMETERS[${atk-1}]}"
+fi
+done
+done
+
+#store last results
+for((  atk = 1 ;  atk <= PAK;  atk++  ))
+do
+RESULT[${atk-1}]=$((RESULT[${atk-1}]+PARAMETERS[${atk-1}]))
+echo "RESULT[${atk-1}]:${RESULT[${atk-1}]}"
+done
+
+#print results
+echo "total queries: $NUMQUERIES"
+for((  atk = 1 ;  atk <= PAK;  atk++  ))
+do
+AVGKSS=`echo "${RESULT[${atk-1}]} / $NUMQUERIES " | bc -l`
+echo "KSS at k=$atk: $AVGKSS " | tee "res.$LOGFILE"
+done
+) < $LOGFILE
+
 
 #### Precision @ k for every prefix length
 echo "precision @ k for every prefix with PFL = $PFL and k up to: $PAK"
@@ -40,7 +76,7 @@ do
 RES=`grep "PREFIXLENGTH: $i " $LOGFILE | grep "RANK: $j" | wc -l`
 HIT=$((RES + HIT))
 precisionAtK=`echo "$HIT / $NUMQUERIES " | bc -l`
-echo "   Precision at k = $j: $precisionAtK PFL = $i" | tee -a "res.$LOGFILE"
+echo "Precision at k=$j with pfl=$i: $precisionAtK" | tee -a "res.$LOGFILE"
 done
 done
 
@@ -59,7 +95,7 @@ HIT=`echo "$HIT + $WRR " | bc -l`
 #HIT=$((HIT + WRR))
 done
 MRR=`echo "$HIT / $NUMQUERIES " | bc -l`
-echo "   MRR = $MRR: PFL = $i" | tee -a "res.$LOGFILE"
+echo "MRR with pfl=$i: $MRR" | tee -a "res.$LOGFILE"
 done
 
 done
