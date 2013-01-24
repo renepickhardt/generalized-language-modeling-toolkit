c81cefd53dc7c95fe7ef2bd44ca2b02614f8959d
koerner <mkoerner@uni-koblenz.de>
2013-01-24 13:25:07 +0100

    added StatisticalHypothesis.java and added standard deviation for KSS

--- src/de/typology/evaluation/Eval.java
+++ src/de/typology/evaluation/Eval.java
@@ -15,13 +15,14 @@ public class Eval {
 	 * @throws IOException 
 	 */
 	public static void main(String[] args) throws IOException {
+		//TODO clean up that mess
 		if(args.length!=2){
 			System.out.println("need parameters: path/to/.logs regular_expression");
-			System.out.println("example for parameters: /home/martin/shells/ trainedOn-wiki-*");
+			System.out.println("example for parameters: /home/martin/results/ trainedOn-wiki-*");
+			return;
 		}
 		BufferedReader reader;
 		BufferedWriter writer;
-		BufferedWriter writer2;
 
 		String path=args[0];
 		File dir=new File(path);
@@ -35,7 +36,6 @@ public class Eval {
 				Double[] parametersDouble={0.0,0.0,0.0,0.0,0.0,0.0};
 				ArrayList<Double[]> results=new ArrayList<Double[]>();
 				writer = IOHelper.openWriteFile(path+"res."+file, 32 * 1024 * 1024);
-				writer2 = IOHelper.openWriteFile(path+"stdev."+file, 32 * 1024 * 1024);
 				reader = IOHelper.openReadFile(path+file);
 				String line;
 				int matchCount=0;
@@ -69,7 +69,9 @@ public class Eval {
 				results.add(parametersDouble.clone());
 				for(int k=1;k<6;k++){
 					resultDouble[k]+=parametersDouble[k];
+					System.out.print(parametersDouble[k]);
 				}
+				System.out.println();
 				//print NKSS to res.
 				for(int k=1;k<6;k++){
 					System.out.println("NKSS at k="+k+": "+resultDouble[k]/matchCount);
@@ -80,12 +82,12 @@ public class Eval {
 				//calculate standard deviation:
 				Double[] stDevPowTwo={0.0,0.0,0.0,0.0,0.0,0.0};
 				Double[] arithMean={0.0,0.0,0.0,0.0,0.0,0.0};
-				int[] CntNotZero={0,0,0,0,0,0};
+				int[] cntNotZero={0,0,0,0,0,0};
 				//count entries that are not zero
 				for(Double[] result:results) {
 					for(int k=1;k<6;k++){
 						if(result[k]>0.0){
-							CntNotZero[k]+=1;
+							cntNotZero[k]+=1;
 						}
 
 					}
@@ -97,8 +99,7 @@ public class Eval {
 					}
 				}
 				for(int k=1;k<6;k++){
-					arithMean[k]/=CntNotZero[k];
-					System.out.println(arithMean[k]);
+					arithMean[k]/=cntNotZero[k];
 				}
 
 				//calculate numerator
@@ -111,7 +112,7 @@ public class Eval {
 				}
 				//divide by count
 				for(int k=1;k<6;k++){
-					stDevPowTwo[k]/=CntNotZero[k];
+					stDevPowTwo[k]=stDevPowTwo[k]/=cntNotZero[k];
 				}
 				//calculate square root
 				Double[] stDev=new Double[6];
@@ -120,25 +121,27 @@ public class Eval {
 				}
 				//print standard deviation to stdev.
 				for(int k=1;k<6;k++){
-					System.out.println("Standard deviation at k="+k+": "+stDev[k]);
-					writer2.write("Standard deviation at k="+k+": "+stDev[k]+"\n");
+					System.out.println("Standard deviation for NKSS at k="+k+": "+stDev[k]);
+					writer.write("Standard deviation for NKSS  at k="+k+": "+stDev[k]+"\n");
 				}
 
-				writer2.close();
 				reader.close();
 
 				//KSS
-				System.out.println("KSS:");
 				reader = IOHelper.openReadFile(path+file);
+				ArrayList<Long[]> resultsLong=new ArrayList<Long[]>();
 				Long[] resultLong={0L,0L,0L,0L,0L,0L};
 				Long[] parametersLong={0L,0L,0L,0L,0L,0L};
 				matchCount=0;
 				while((line=reader.readLine()) != null){
 					if(line.contains("MATCH")){
 						matchCount++;
+						resultsLong.add(parametersLong.clone());
 						for(int k=1;k<6;k++){
 							resultLong[k]+=parametersLong[k];
+							//	System.out.print(parametersLong[k]+" ");
 						}
+						//System.out.println();
 						for(int i=1;i<parametersLong.length;i++){
 							parametersLong[i]=0L;
 						}
@@ -156,14 +159,66 @@ public class Eval {
 					}
 				}
 				//store last parameters
+				resultsLong.add(parametersLong.clone());
 				for(int k=1;k<6;k++){
 					resultLong[k]+=parametersLong[k];
+					//System.out.print(parametersLong[k]+" ");
 				}
+				//System.out.println();
 				for(int k=1;k<6;k++){
-					System.out.println("KSS at k="+k+": "+resultLong[k]+"/"+matchCount+"="+(double)resultLong[k]/matchCount);
+					//	System.out.println("KSS at k="+k+": "+resultLong[k]+"/"+matchCount+"="+(double)resultLong[k]/matchCount);
 					writer.write("KSS at k="+k+": "+(double)resultLong[k]/matchCount+"\n");
 				}
 
+				//calculate standard deviation:
+				Double[] stDevPowTwo2={0.0,0.0,0.0,0.0,0.0,0.0};
+				Double[] arithMean2={0.0,0.0,0.0,0.0,0.0,0.0};
+				int[] cntNotZero2={0,0,0,0,0,0};
+				//count entries that are not zero
+				for(Long[] result:resultsLong) {
+					for(int k=1;k<6;k++){
+						if(result[k]>0.0){
+							cntNotZero2[k]+=1;
+						}
+
+					}
+				}
+				//calculate arithmetic mean
+				for(Long[] result:resultsLong) {
+					for(int k=1;k<6;k++){
+						arithMean2[k]+=result[k];
+					}
+				}
+				for(int k=1;k<6;k++){
+					//System.out.println(arithMean2[k]+"/="+cntNotZero2[k]+"="+(arithMean2[k]/=cntNotZero2[k]));
+					arithMean2[k]/=cntNotZero2[k];
+				}
+
+				//calculate numerator
+				for(Long[] result:resultsLong) {
+					for(int k=1;k<6;k++){
+						if(result[k]>0.0){
+							//	System.out.println(k+"=("+result[k]+"-"+arithMean2[k]+")²="+Math.pow(result[k]-arithMean2[k],2));
+							stDevPowTwo2[k]+=Math.pow(result[k]-arithMean2[k], 2);
+						}
+					}
+				}
+				//divide by count
+				for(int k=1;k<6;k++){
+					//	System.out.println(stDevPowTwo2[k]+"/="+cntNotZero2[k]+"="+stDevPowTwo2[k]/cntNotZero2[k]);
+					stDevPowTwo2[k]=stDevPowTwo2[k]/=cntNotZero2[k];
+				}
+				//calculate square root
+				Double[] stDev2=new Double[6];
+				for(int k=1;k<6;k++){
+					//	System.out.println(k+"=sqrt("+stDevPowTwo2[k]+")="+Math.sqrt(stDevPowTwo2[k]));
+					stDev2[k]=Math.sqrt(stDevPowTwo2[k]);
+				}
+				//print standard deviation to stdev.
+				for(int k=1;k<6;k++){
+					//	System.out.println("Standard deviation for KSS at k="+k+": "+stDev2[k]);
+					writer.write("Standard deviation for KSS  at k="+k+": "+stDev2[k]+"\n");
+				}
 
 
 				reader.close();
