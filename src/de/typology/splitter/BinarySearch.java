c21b7f389ae0a1c80227ca41d6fdcd95a29246c7
koerner <mkoerner@uni-koblenz.de>
2013-03-25 15:01:11 +0100

    reorganized some libraries, added some tests

--- src/de/typology/splitter/BinarySearch.java
+++ src/de/typology/splitter/BinarySearch.java
@@ -11,25 +11,13 @@ public class BinarySearch {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// String[] test = { "ab", "dr", "te", "yr" };
+		//
 		// System.out.println("$ads: " + BinarySearch.rank("$ads", test) +
 		// "=0");
 		// System.out.println("00: " + BinarySearch.rank("00", test) + "=0");
 		// System.out.println("aabc: " + BinarySearch.rank("aabc", test) +
 		// "=0");
-		// System.out.println("ab: " + BinarySearch.rank("ab", test) + "=0");
-		// System.out.println("aba: " + BinarySearch.rank("aba", test) + "=0");
-		// System.out.println("abz: " + BinarySearch.rank("abz", test) + "=0");
-		// System.out.println("abA: " + BinarySearch.rank("abA", test) + "=0");
-		// System.out.println("abZ: " + BinarySearch.rank("abZ", test) + "=0");
-		// System.out.println("dqz: " + BinarySearch.rank("dqz", test) + "=0");
-		// System.out.println("ds: " + BinarySearch.rank("ds", test) + "=1");
-		// System.out.println("er: " + BinarySearch.rank("er", test) + "=1");
-		// System.out.println("td: " + BinarySearch.rank("td", test) + "=1");
-		// System.out.println("yqz: " + BinarySearch.rank("yqz", test) + "=2");
-		// System.out.println("yr: " + BinarySearch.rank("yr", test) + "=3");
-		// System.out.println("yra: " + BinarySearch.rank("yra", test) + "=3");
-		// System.out.println("za: " + BinarySearch.rank("za", test) + "=3");
+
 		String outputDirectory = Config.get().outputDirectory
 				+ Config.get().inputDataSet;
 		IndexBuilder ib = new IndexBuilder();
