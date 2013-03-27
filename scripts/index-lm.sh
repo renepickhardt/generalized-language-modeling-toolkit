68c84c4b8c4e868d41d1881924d49ae03ebb9ccd
koerner <mkoerner@uni-koblenz.de>
2013-03-27 20:08:28 +0100

    updated index-lm.sh and index-typo.sh and added support for 0es and 1gs

--- scripts/index-lm.sh
+++ scripts/index-lm.sh
@@ -4,107 +4,108 @@ testName="hybridngram"
 #dbPath="/mnt/vdb/typoeval/mysql/${testName}/" #server
 dbPath=/var/lib/mysql/${testName}/ #local machine
 
-#mysql -u ${dbUser} -e "create database ${testName};"
+mysql -u ${dbUser} -e "drop database ${testName};"
+mysql -u ${dbUser} -e "create database ${testName};"
 
-for (( i = 2 ;  i <= 5;  i++  ))
-do
-path=$1$i"gs/*"
-for file in $path
+for (( i = 1 ;  i <= 5;  i++  ))
+  do
+  path=$1$i"gs/*"
+  for file in $path
+    do
+    xpath=${file%/*}
+    xbase=${file##*/}
+    xfext=${xbase##*.}
+    xpref=${xbase%.*}
+    tablename=$xfext"_"$xpref;
+    echo "tablename: "$tablename;
+    echo "xpath: "$xpath;
+    echo "xbase: "$xbase;
+    echo "xfext: "$xfext;
+    echo "xpref: "$xpref;
 
-do
-xpath=${file%/*} 
-xbase=${file##*/}
-xfext=${xbase##*.}
-xpref=${xbase%.*}
-tablename=$xfext"_"$xpref;
-echo $tablename;
-echo $xpath;
-echo $xbase;
-echo $xfext;
-echo $xpref;
+   #create tables and indices
+   if [ $i -eq 1 ];
+      then
+      mysql -u ${dbUser} $testName --local-infile=1 -e "create table ${tablename} (target varchar(60),score float) engine=myisam character set utf8 collate utf8_bin;
+      create index ${tablename}_ix on ${tablename} (target(1), score desc);
+      create index ${tablename}_2_ix on ${tablename} (target(2), score desc);
+      create index ${tablename}_3_ix on ${tablename} (target(3), score desc);
+      create index ${tablename}_4_ix on ${tablename} (target(4), score desc);
+      create index ${tablename}_5_ix on ${tablename} (target(5), score desc);"
+    fi
 
-#create tables and indices
-if [ $i -eq 2 ];
-then 
-mysql -u ${dbUser} $testName --local-infile=1 -e "create table ${tablename} (source1 varchar(60),target varchar(60),score float) engine=myisam character set utf8 collate utf8_bin;
-create index ${tablename}_ix on ${tablename} (source1(60), score desc);
-create index ${tablename}_2_ix on ${tablename} (source1(60), target(2));
-create index ${tablename}_3_ix on ${tablename} (source1(60), target(3));
-create index ${tablename}_4_ix on ${tablename} (source1(60), target(4));
-create index ${tablename}_5_ix on ${tablename} (source1(60), target(5));"
-fi
+    if [ $i -eq 2 ];
+      then
+      mysql -u ${dbUser} $testName --local-infile=1 -e "create table ${tablename} (source1 varchar(60),target varchar(60),score float) engine=myisam character set utf8 collate utf8_bin;
+      create index ${tablename}_ix on ${tablename} (source1(60), score desc);
+      create index ${tablename}_2_ix on ${tablename} (source1(60), target(2));
+      create index ${tablename}_3_ix on ${tablename} (source1(60), target(3));
+      create index ${tablename}_4_ix on ${tablename} (source1(60), target(4));
+      create index ${tablename}_5_ix on ${tablename} (source1(60), target(5));"
+    fi
 
-if [ $i -eq 3 ];
-then 
-mysql -u ${dbUser} $testName --local-infile=1 -e "create table ${tablename} (source1 varchar(60),source2 varchar(60),target varchar(60),score float) engine=myisam character set utf8 collate utf8_bin;
-create index ${tablename}_ix on ${tablename} (source1(60), source2(60), score desc);
-create index ${tablename}_2_ix on ${tablename} (source1(60), source2(60), target(2));
-create index ${tablename}_3_ix on ${tablename} (source1(60), source2(60), target(3));
-create index ${tablename}_4_ix on ${tablename} (source1(60), source2(60), target(4));
-create index ${tablename}_5_ix on ${tablename} (source1(60), source2(60), target(5));"
-fi
+    if [ $i -eq 3 ];
+      then
+      mysql -u ${dbUser} $testName --local-infile=1 -e "create table ${tablename} (source1 varchar(60),source2 varchar(60),target varchar(60),score float) engine=myisam character set utf8 collate utf8_bin;
+      create index ${tablename}_ix on ${tablename} (source1(60), source2(60), score desc);
+      create index ${tablename}_2_ix on ${tablename} (source1(60), source2(60), target(2));
+      create index ${tablename}_3_ix on ${tablename} (source1(60), source2(60), target(3));
+      create index ${tablename}_4_ix on ${tablename} (source1(60), source2(60), target(4));
+      create index ${tablename}_5_ix on ${tablename} (source1(60), source2(60), target(5));"
+    fi
 
-if [ $i -eq 4 ];
-then 
-mysql -u ${dbUser} $testName --local-infile=1 -e "create table ${tablename} (source1 varchar(60),source2 varchar(60), source3 varchar(60), target varchar(60),score float) engine=myisam character set utf8 collate utf8_bin;
-create index ${tablename}_ix on ${tablename} (source1(60), source2(60), source3(60), score desc);
-create index ${tablename}_2_ix on ${tablename} (source1(60), source2(60), source3(60), target(2));
-create index ${tablename}_3_ix on ${tablename} (source1(60), source2(60), source3(60), target(3));
-create index ${tablename}_4_ix on ${tablename} (source1(60), source2(60), source3(60), target(4));
-create index ${tablename}_5_ix on ${tablename} (source1(60), source2(60), source3(60), target(5));"
-fi
+    if [ $i -eq 4 ];
+      then
+      mysql -u ${dbUser} $testName --local-infile=1 -e "create table ${tablename} (source1 varchar(60),source2 varchar(60), source3 varchar(60), target varchar(60),score float) engine=myisam character set utf8 collate utf8_bin;
+      create index ${tablename}_ix on ${tablename} (source1(60), source2(60), source3(60), score desc);
+      create index ${tablename}_2_ix on ${tablename} (source1(60), source2(60), source3(60), target(2));
+      create index ${tablename}_3_ix on ${tablename} (source1(60), source2(60), source3(60), target(3));
+      create index ${tablename}_4_ix on ${tablename} (source1(60), source2(60), source3(60), target(4));
+      create index ${tablename}_5_ix on ${tablename} (source1(60), source2(60), source3(60), target(5));"
+    fi
 
-if [ $i -eq 5 ];
-then 
-mysql -u ${dbUser} $testName --local-infile=1 -e "create table ${tablename} (source1 varchar(60),source2 varchar(60), source3 varchar(60), source4 varchar(60), target varchar(60),score float) engine=myisam character set utf8 collate utf8_bin;
-create index ${tablename}_ix on ${tablename} (source1(60), source2(60), source3(60),  source4(60), score desc);
-create index ${tablename}_2_ix on ${tablename} (source1(60), source2(60), source3(60), source4(60), target(2));
-create index ${tablename}_3_ix on ${tablename} (source1(60), source2(60), source3(60), source4(60), target(3));
-create index ${tablename}_4_ix on ${tablename} (source1(60), source2(60), source3(60), source4(60), target(4));
-create index ${tablename}_5_ix on ${tablename} (source1(60), source2(60), source3(60), source4(60), target(5));"
+    if [ $i -eq 5 ];
+      then
+      mysql -u ${dbUser} $testName --local-infile=1 -e "create table ${tablename} (source1 varchar(60),source2 varchar(60), source3 varchar(60), source4 varchar(60), target varchar(60),score float) engine=myisam character set utf8 collate utf8_bin;
+      create index ${tablename}_ix on ${tablename} (source1(60), source2(60), source3(60),  source4(60), score desc);
+      create index ${tablename}_2_ix on ${tablename} (source1(60), source2(60), source3(60), source4(60), target(2));
+      create index ${tablename}_3_ix on ${tablename} (source1(60), source2(60), source3(60), source4(60), target(3));
+      create index ${tablename}_4_ix on ${tablename} (source1(60), source2(60), source3(60), source4(60), target(4));
+      create index ${tablename}_5_ix on ${tablename} (source1(60), source2(60), source3(60), source4(60), target(5));"
+    fi
 
-fi
+    #disable indices
+    myisamchk --keys-used=0 -rq ${dbPath}${tablename}
+    mysql -u ${dbUser} $testName --local-infile=1 -e "flush tables;"
 
+    #import data
+    if [ $i -eq 2 ];
+      then
+      mysql -u ${dbUser} $testName --local-infile=1 -e "load data local infile '$file' into table ${tablename} fields terminated by '\t' enclosed by '' lines terminated by '\n' (source1, target, score);"
+    fi
 
-#disable indices
-myisamchk --keys-used=0 -rq ${dbPath}${tablename}
+    if [ $i -eq 3 ];
+      then
+      mysql -u ${dbUser} $testName --local-infile=1 -e "load data local infile '$file' into table ${tablename} fields terminated by '\t' enclosed by '' lines terminated by '\n' (source1, source2, target, score);"
+    fi
 
-mysql -u ${dbUser} $testName --local-infile=1 -e "flush tables;"
+    if [ $i -eq 4 ];
+      then
+      mysql -u ${dbUser} $testName --local-infile=1 -e "load data local infile '$file' into table ${tablename} fields terminated by '\t' enclosed by '' lines terminated by '\n' (source1, source2, source3, target, score);"
+    fi
 
-#remove hash (#) from the score and save file to /dev/shm for faster processing
-sed 's/#//g' $file > /dev/shm/tmp.typoedges
+    if [ $i -eq 5 ];
+      then
+      mysql -u ${dbUser} $testName --local-infile=1 -e "load data local infile '$file' into table ${tablename} fields terminated by '\t' enclosed by '' lines terminated by '\n' (source1, source2, source3, source4, target, score);"
+    fi
 
-#import data
-if [ $i -eq 2 ];
-then 
-mysql -u ${dbUser} $testName --local-infile=1 -e "load data local infile '/dev/shm/tmp.typoedges' into table ${tablename} fields terminated by '\t' enclosed by '' lines terminated by '\n' (source1, target, score);"
-fi
+    #compress table / really necessary?
+    myisampack ${dbPath}${tablename}
 
-if [ $i -eq 3 ];
-then 
-mysql -u ${dbUser} $testName --local-infile=1 -e "load data local infile '/dev/shm/tmp.typoedges' into table ${tablename} fields terminated by '\t' enclosed by '' lines terminated by '\n' (source1, source2, target, score);"
-fi
+    #enable index
+    myisamchk -rq ${dbPath}${tablename} --sort_buffer=3G #--sort-index --sort-records=1
 
-if [ $i -eq 4 ];
-then 
-mysql -u ${dbUser} $testName --local-infile=1 -e "load data local infile '/dev/shm/tmp.typoedges' into table ${tablename} fields terminated by '\t' enclosed by '' lines terminated by '\n' (source1, source2, source3, target, score);"
-fi
-
-if [ $i -eq 5 ];
-then 
-mysql -u ${dbUser} $testName --local-infile=1 -e "load data local infile '/dev/shm/tmp.typoedges' into table ${tablename} fields terminated by '\t' enclosed by '' lines terminated by '\n' (source1, source2, source3, source4, target, score);"
-fi
-
-
-#compress table / really necessary?
-myisampack ${dbPath}${tablename}
-
-#enable index
-myisamchk -rq ${dbPath}${tablename} --sort_buffer=3G #--sort-index --sort-records=1
-
-#and flush index again.
-mysql -u ${dbUser} $testName --local-infile=1 -e "flush tables;"
-
+    #and flush index again
+    mysql -u ${dbUser} $testName --local-infile=1 -e "flush tables;"
+  done;
 done;
-done;
-rm /dev/shm/tmp.typoedges
--- scripts/index-typo.sh
+++ scripts/index-typo.sh
@@ -4,54 +4,47 @@ testName="hybridtypology"
 #dbPath="/mnt/vdb/typoeval/mysql/${testName}/" #server
 dbPath=/var/lib/mysql/${testName}/ #local machine
 
+mysql -u ${dbUser} -e "drop database ${testName};"
 mysql -u ${dbUser} -e "create database ${testName};"
 
 path="$1*/*"
 for file in $path
+  do
+  xpath=${file%/*}
+  xbase=${file##*/}
+  xfext=${xbase##*.}
+  xpref=${xbase%.*}
+  tablename=$xfext"_"$xpref;
+  echo "tablename: "$tablename;
+  echo "xpath: "$xpath;
+  echo "xbase: "$xbase;
+  echo "xfext: "$xfext;
+  echo "xpref: "$xpref;
 
-do
-xpath=${file%/*} 
-xbase=${file##*/}
-xfext=${xbase##*.}
-xpref=${xbase%.*}
-tablename=$xfext"_"$xpref;
-echo $tablename;
-echo $xpath;
-echo $xbase;
-echo $xfext;
-echo $xpref;
-
-
-
-#create tables and indices
-mysql -u ${dbUser} $testName --local-infile=1 -e "create table ${tablename} (source varchar(60),target varchar(60),score float) engine=myisam character set utf8 collate utf8_bin;
-create index ${tablename}_ix on ${tablename} (source(60), score desc);
-create index ${tablename}_2_ix on ${tablename} (source(60), target(2));
-create index ${tablename}_3_ix on ${tablename} (source(60), target(3));
-create index ${tablename}_4_ix on ${tablename} (source(60), target(4));
-create index ${tablename}_5_ix on ${tablename} (source(60), target(5));"
+  #create tables and indices
+  mysql -u ${dbUser} $testName --local-infile=1 -e "create table ${tablename} (source varchar(60),target varchar(60),score float) engine=myisam character set utf8 collate utf8_bin;
+  create index ${tablename}_ix on ${tablename} (source(60), score desc);
+  create index ${tablename}_2_ix on ${tablename} (source(60), target(2));
+  create index ${tablename}_3_ix on ${tablename} (source(60), target(3));
+  create index ${tablename}_4_ix on ${tablename} (source(60), target(4));
+  create index ${tablename}_5_ix on ${tablename} (source(60), target(5));"
 
 
-#disable indices
-myisamchk --keys-used=0 -rq ${dbPath}${tablename}
+  #disable indices
+  myisamchk --keys-used=0 -rq ${dbPath}${tablename}
 
-mysql -u ${dbUser} $testName --local-infile=1 -e "flush tables;"
+  mysql -u ${dbUser} $testName --local-infile=1 -e "flush tables;"
 
-#remove hash (#) from the score and save file to /dev/shm for faster processing
-sed 's/#//g' $file > /dev/shm/tmp.typoedges
+  #import data
+  mysql -u ${dbUser} $testName --local-infile=1 -e "load data local infile '$file' into table ${tablename} fields terminated by '\t' enclosed by '' lines terminated by '\n' (source, target, score);"
 
-#import data
-mysql -u ${dbUser} $testName --local-infile=1 -e "load data local infile '/dev/shm/tmp.typoedges' into table ${tablename} fields terminated by '\t' enclosed by '' lines terminated by '\n' (source, target, score);"
+  #compress table / really necessary?
+  myisampack ${dbPath}${tablename}
 
-#compress table / really necessary?
-myisampack ${dbPath}${tablename}
+  #enable index
+  myisamchk -rq ${dbPath}${tablename} --sort_buffer=3G #--sort-index --sort-records=1
 
-#enable index
-myisamchk -rq ${dbPath}${tablename} --sort_buffer=3G #--sort-index --sort-records=1
-
-#and flush index again.
-mysql -u ${dbUser} $testName --local-infile=1 -e "flush tables;"
+  #and flush index again.
+  mysql -u ${dbUser} $testName --local-infile=1 -e "flush tables;"
 
 done
-
-rm /dev/shm/tmp.typoedges
