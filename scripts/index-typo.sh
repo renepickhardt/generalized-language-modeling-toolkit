da82093671a211b2e2bc37e59cae209a5ee00fc3
Rene Pickhardt <rene@rene-pickhardt.de>
2013-04-09 12:52:31 +0200

    updated script for indexing typology

--- scripts/index-typo.sh
+++ scripts/index-typo.sh
@@ -24,10 +24,10 @@ for file in $path
   #create tables and indices
   mysql -u ${dbUser} $testName --local-infile=1 -e "create table ${tablename} (source varchar(60),target varchar(60),score float) engine=myisam character set utf8 collate utf8_bin;
   create index ${tablename}_ix on ${tablename} (source(60), score desc);
-  create index ${tablename}_2_ix on ${tablename} (source(60), target(2));
-  create index ${tablename}_3_ix on ${tablename} (source(60), target(3));
-  create index ${tablename}_4_ix on ${tablename} (source(60), target(4));
-  create index ${tablename}_5_ix on ${tablename} (source(60), target(5));"
+  create index ${tablename}_2_ix on ${tablename} (source(60), target(2), score desc);
+  create index ${tablename}_3_ix on ${tablename} (source(60), target(3), score desc);
+  create index ${tablename}_4_ix on ${tablename} (source(60), target(4), score desc);
+  create index ${tablename}_5_ix on ${tablename} (source(60), target(5), score desc);"
 
 
   #disable indices
