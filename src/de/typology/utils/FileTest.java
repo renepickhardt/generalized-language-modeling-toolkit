5ff9c14ba9bc20956cb33f53220380a9f076ab0b
mkrnr <info@mkoerner.de>
2013-04-19 12:27:25 +0200

    Add FileTest for debugging reasons

---
+++ src/de/typology/utils/FileTest.java
@@ -0,0 +1,18 @@
+package de.typology.utils;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class FileTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		ArrayList<File> files = new ArrayList<File>();
+		for (int i = 0; i < Config.get().fileSizeThreashhold; i++) {
+			System.out.println(i);
+			files.add(new File("/home/martin/test" + i));
+		}
+	}
+}
