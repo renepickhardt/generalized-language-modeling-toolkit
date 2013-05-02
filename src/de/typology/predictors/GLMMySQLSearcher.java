13be4f63dcfe1d4b3d9004dc4d427a46ac2a2d0f
mkrnr <info@mkoerner.de>
2013-05-02 15:16:19 +0200

    Fix sequence == 1 removal

--- src/de/typology/predictors/GLMMySQLSearcher.java
+++ src/de/typology/predictors/GLMMySQLSearcher.java
@@ -52,12 +52,10 @@ public class GLMMySQLSearcher extends NewMySQLSearcher {
 		int leadingZeros = 0;
 		if (sequence == 1) {
 			source = "true";
+			// TODO:remove this:
+			return null;
 		} else {
 
-			// TODO: remove this:
-			if (sequence == 1) {
-				return null;
-			}
 			if (sequence % 2 == 0) {
 				// no target in sequence (e.g. 110)
 				return null;
@@ -119,6 +117,7 @@ public class GLMMySQLSearcher extends NewMySQLSearcher {
 			query = "select * from " + tableName + " where " + source
 					+ " order by score desc limit " + this.joinLength;
 		}
+		System.out.println(query);
 		return query;
 	}
 }
