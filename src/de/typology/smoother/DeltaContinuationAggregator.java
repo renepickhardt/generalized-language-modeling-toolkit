642ff7dfd36caabd10db2a9640259cab5f44e505
mkrnr <info@mkoerner.de>
2013-05-17 21:07:07 +0200

    Add DeltaContinuationAggregator.java (empty yet)

---
+++ src/de/typology/smoother/DeltaContinuationAggregator.java
@@ -0,0 +1,20 @@
+package de.typology.smoother;
+
+public class DeltaContinuationAggregator {
+
+	/**
+	 * This class provides a method that aggregates and calculates the delta and
+	 * continuation probability based on a higher order glm.
+	 * <p>
+	 * TODO: add extended Kneser-Ney smoothing at some point here
+	 * 
+	 * @param args
+	 * 
+	 * @author Martin Koerner
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
