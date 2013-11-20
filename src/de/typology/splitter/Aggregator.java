72cefe0cc8e3625e0a6fa10d7ca8cd35cbcd5587
mkrnr <info@mkoerner.de>
2013-11-20 15:31:02 +0100

    Modify Aggregator to use less space by replacing List<String> with String

--- src/de/typology/splitter/Aggregator.java
+++ src/de/typology/splitter/Aggregator.java
@@ -1,12 +1,14 @@
 package de.typology.splitter;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Arrays;
+import java.security.InvalidParameterException;
 import java.util.Comparator;
-import java.util.List;
+import java.util.Map.Entry;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
@@ -51,84 +53,66 @@ public class Aggregator implements Runnable {
 					this.inputFile));
 
 			// this comparator is based on the value of startSortAtColumn
-			Comparator<List<String>> arrayComparator = new Comparator<List<String>>() {
+			Comparator<String> stringComparator = new Comparator<String>() {
 				@Override
-				public int compare(List<String> strings1, List<String> strings2) {
-					// start comparison at "startSortAtColumn"
-					if (strings1.size() != strings2.size()) {
-						System.out.println("ERROR: " + strings1.size() + "!="
-								+ strings2.size());
-						for (int i = 0; i < strings1.size(); i++) {
-							System.out.print(strings1.get(i) + " ");
-						}
-						System.out.println();
-						for (int i = 0; i < strings2.size(); i++) {
-							System.out.print(strings2.get(i) + " ");
-						}
-						System.out.println();
-					}
-					for (int i = Aggregator.this.startSortAtColumn; i < strings1
-							.size(); i++) {
-						if (!strings1.get(i).equals(strings2.get(i))) {
-							return strings1.get(i).compareTo(strings2.get(i));
-						}
-					}
-					// all columns starting at "startSortAtColumn" are equal so
-					// check the columns before "startSortAtColumn"
-					for (int i = 0; i < Aggregator.this.startSortAtColumn; i++) {
-						if (!strings1.get(i).equals(strings2.get(i))) {
-							return strings1.get(i).compareTo(strings2.get(i));
+				public int compare(String string1, String string2) {
+					if (Aggregator.this.startSortAtColumn == 0) {
+						return string1.compareTo(string2);
+					} else {
+						if (Aggregator.this.startSortAtColumn == 1) {
+							int result = string1.substring(
+									string1.indexOf(' ') + 1)
+									.compareTo(
+											string2.substring(string2
+													.indexOf(' ') + 1));
+							if (result != 0) {
+								return result;
+							} else {
+								return string1.compareTo(string2);
+							}
+						} else {
+							throw new InvalidParameterException(
+									"startSortAtColumn greater than 1 is not implemented");
 						}
 					}
-					return 0;
 				}
 			};
-			SortedMap<List<String>, Long> wordMap = new TreeMap<List<String>, Long>(
-					arrayComparator);
-			// HashMap<String, Long> wordMap = new HashMap<String, Long>();
+			SortedMap<String, Long> wordMap = new TreeMap<String, Long>(
+					stringComparator);
 			String inputLine;
 			// TODO remove
 			System.gc();
 			System.out.println("before reading " + this.inputFile.getName()
 					+ ":");
 			this.printMemory();
+
 			while ((inputLine = inputFileReader.readLine()) != null) {
 
-				List<String> words = Arrays.asList(inputLine
-						.split(this.delimiter)[0].split("\\s"));
-				// String words = inputLine.split(this.delimiter)[0];
+				String words = inputLine.split(this.delimiter)[0];
 				long count = Long.parseLong(inputLine.split(this.delimiter)[1]);
-				if (words.size() == 0) {
+				if (words.length() == 0) {
 					// logger.error("empty row in " + this.inputFile + ": \""
 					// + inputLine + "\"");
 					// logger.error("exiting JVM");
 					// System.exit(1);
 					continue;
 				}
-				try {
-					if (wordMap.containsKey(words)) {
-						// System.out.println();
-						// System.out.print("IN: ");
-						// for (String s : words) {
-						// System.out.print(s + " ");
-						// }
-						// System.out.println();
-						wordMap.put(words, wordMap.get(words) + count);
-					} else {
-						// System.out.println();
-						// System.out.print("NOT: ");
-						// for (String s : words) {
-						// System.out.print(s + " ");
-						// }
-						// System.out.println();
-						wordMap.put(words, count);
-					}
-				} catch (ArrayIndexOutOfBoundsException e) {
-					// TODO remove this
-					System.out.println("inputLine: " + inputLine);
-					System.out.println(words);
-					System.out.println(this.inputFile.getAbsolutePath());
-					e.printStackTrace();
+				if (wordMap.containsKey(words)) {
+					// System.out.println();
+					// System.out.print("IN: ");
+					// for (String s : words) {
+					// System.out.print(s + " ");
+					// }
+					// System.out.println();
+					wordMap.put(words, wordMap.get(words) + count);
+				} else {
+					// System.out.println();
+					// System.out.print("NOT: ");
+					// for (String s : words) {
+					// System.out.print(s + " ");
+					// }
+					// System.out.println();
+					wordMap.put(words, count);
 				}
 			}
 			// TODO remove
@@ -136,20 +120,18 @@ public class Aggregator implements Runnable {
 					+ ":");
 			this.printMemory();
 			System.gc();
+
 			inputFileReader.close();
-			// BufferedWriter outputFileWriter = new BufferedWriter(
-			// new FileWriter(this.outputFile));
-			// for (Entry<List<String>, Long> entry : wordMap.entrySet()) {
-			// List<String> words = entry.getKey();
-			// for (int i = 0; i < words.size() - 1; i++) {
-			// outputFileWriter.write(words.get(i) + " ");
-			// }
-			// outputFileWriter.write(words.get(words.size() - 1));
-			//
-			// outputFileWriter
-			// .write(this.delimiter + entry.getValue() + "\n");
-			// }
-			// outputFileWriter.close();
+			BufferedWriter outputFileWriter = new BufferedWriter(
+					new FileWriter(this.outputFile));
+			for (Entry<String, Long> entry : wordMap.entrySet()) {
+				String words = entry.getKey();
+				outputFileWriter.write(words);
+
+				outputFileWriter
+						.write(this.delimiter + entry.getValue() + "\n");
+			}
+			outputFileWriter.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
