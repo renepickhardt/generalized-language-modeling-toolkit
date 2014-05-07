9f05b77c8a74a9f3a3ba294e485ff520df4a3dc6
Rene Pickhardt <rene@rene-pickhardt.de>
2014-05-07 15:33:00 +0200

    commeted out lines

--- src/test/java/de/typology/patterns/PatternTest.java
+++ src/test/java/de/typology/patterns/PatternTest.java
@@ -24,18 +24,18 @@ public class PatternTest {
 		
 		System.out.println("missing SKP pattern");
 		
-		List<PatternType> pattern = new ArrayList<PatternType>();
-		pattern.add(PatternType.POS);
-		pattern.add(PatternType.DEL);
-		pattern.add(PatternType.SKP);
-		pattern.add(PatternType.CNT);
-		Pattern p = new Pattern(pattern);
-		//TODO: I also don't expect 
-		assertTrue(p.numCnt()==1);
-		System.out.println(p.toString());
-		//TODO: I don't understand this method (I would expect that we need without Del and not without Skp)
-		p = Pattern.newWithoutSkp(p);
-		System.out.println(p.toString());
+//		List<PatternType> pattern = new ArrayList<Pattern>();
+//		pattern.add(PatternType.POS);
+//		pattern.add(PatternType.DEL);
+//		pattern.add(PatternType.SKP);
+//		pattern.add(PatternType.CNT);
+//		Pattern p = new Pattern(pattern);
+//		//TODO: I also don't expect 
+//		assertTrue(p.numCnt()==1);
+//		System.out.println(p.toString());
+//		//TODO: I don't understand this method (I would expect that we need without Del and not without Skp)
+//		p = Pattern.newWithoutSkp(p);
+//		System.out.println(p.toString());
 		
 	}
 
