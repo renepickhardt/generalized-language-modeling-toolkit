88784215529f7800be3725b129226d0b79f16eae
Lukas Schmelzeisen <l.schmelzeisen@gmx.de>
2014-05-15 23:01:28 +0200

    Moved abc props to test.

--- src/test/java/de/typology/smoothing/SmootherTest.java
+++ src/test/java/de/typology/smoothing/SmootherTest.java
@@ -1,11 +1,86 @@
 package de.typology.smoothing;
 
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class SmootherTest extends AbcCorpusTest {
 
     @Test
-    public void test() {
+    @Ignore
+    public void printInterpolatedKneserNey() throws IOException {
+        Smoother smoother =
+                new InterpolatedKneserNeySmoother(abcAbsoluteDir,
+                        abcContinuationDir, "\t");
+
+        Map<Integer, Map<String, Double>> propabilitiesByLength =
+                new LinkedHashMap<Integer, Map<String, Double>>();
+        for (int i = 1; i != 6; ++i) {
+            propabilitiesByLength
+                    .put(i, calcSequencePropabilities(smoother, i));
+        }
+
+        for (Map<String, Double> propabilities : propabilitiesByLength.values()) {
+            System.out.println("---");
+            printPropabilities(propabilities);
+        }
+    }
+
+    private Map<String, Double> calcSequencePropabilities(
+            Smoother smooter,
+            int length) {
+        Map<String, Double> propabilities = new LinkedHashMap<String, Double>();
+
+        for (int i = 0; i != ((int) Math.pow(3, length)); ++i) {
+            String sequence = getAbcSequence(i, length);
+            propabilities.put(sequence, smooter.propability(sequence));
+        }
+
+        return propabilities;
+    }
+
+    private void printPropabilities(Map<String, Double> propabilities) {
+        double sum = 0;
+        for (Map.Entry<String, Double> sequencePropability : propabilities
+                .entrySet()) {
+            String sequence = sequencePropability.getKey();
+            double propability = sequencePropability.getValue();
+
+            sum += propability;
+
+            System.out.println(sequence + " ->" + propability);
+        }
+        System.out.println("sum = " + sum);
+    }
+
+    private String getAbcSequence(int num, int length) {
+        StringBuilder result = new StringBuilder();
+
+        boolean frist = true;
+        for (int k = 0; k != length; ++k) {
+            if (frist) {
+                frist = false;
+            } else {
+                result.append(" ");
+            }
+            switch (num % 3) {
+                case 0:
+                    result.append("a");
+                    break;
+                case 1:
+                    result.append("b");
+                    break;
+                case 2:
+                    result.append("c");
+                    break;
+            }
+            num /= 3;
+        }
+
+        return result.reverse().toString();
     }
 
 }
