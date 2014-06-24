011799a63bca8f7e51e00f62a1a3c55f8d61ca22
Lukas Schmelzeisen <l.schmelzeisen@gmx.de>
2014-06-24 14:19:40 +0200

    Added new executables. Added new config. Removed old stuff.

---
+++ src/main/java/de/typology/executables/Glmtk.java
@@ -0,0 +1,9 @@
+package de.typology.executables;
+
+public class Glmtk {
+
+    public static void main(String[] args) {
+        System.out.println("Glmtk");
+    }
+
+}
---
+++ src/main/java/de/typology/executables/GlmtkCount.java
@@ -0,0 +1,9 @@
+package de.typology.executables;
+
+public class GlmtkCount {
+
+    public static void main(String[] args) {
+        System.out.println("GlmtkCount");
+    }
+
+}
