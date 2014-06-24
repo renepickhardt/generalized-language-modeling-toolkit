d5bb343ad6a799ee6567a52fef40b73a6fc30c59
Lukas Schmelzeisen <l.schmelzeisen@gmx.de>
2014-06-24 22:14:47 +0200

    Now logging current git commit.

--- src/main/java/de/typology/executables/Executable.java
+++ src/main/java/de/typology/executables/Executable.java
@@ -1,6 +1,8 @@
 package de.typology.executables;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.nio.file.Path;
@@ -33,6 +35,19 @@ public abstract class Executable {
 
             logger.info("Starting " + getClass().getSimpleName() + ".");
 
+            // log Git Commit
+            Process gitLogProc = Runtime.getRuntime().exec(new String[] {
+                "git", "log", "-1", "--format=%H: %s"
+            });
+            gitLogProc.waitFor();
+            try (BufferedReader gitLogReader =
+                    new BufferedReader(new InputStreamReader(
+                            gitLogProc.getInputStream()))) {
+                String gitCommit = gitLogReader.readLine();
+                logger.info("Git Commit: " + gitCommit);
+            }
+
+            // log Config
             config = Config.get();
             logger.info("Config: " + config);
 
