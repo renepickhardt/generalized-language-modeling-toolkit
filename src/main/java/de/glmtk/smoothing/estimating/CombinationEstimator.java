b5d79151cde4eca43de576d3f89353a952efcce7
Lukas Schmelzeisen <l.schmelzeisen@gmx.de>
2014-07-23 17:37:59 +0200

    Implemented CombinationEstimator.

---
+++ src/main/java/de/glmtk/smoothing/estimating/CombinationEstimator.java
@@ -0,0 +1,40 @@
+package de.glmtk.smoothing.estimating;
+
+import de.glmtk.smoothing.Corpus;
+import de.glmtk.smoothing.NGram;
+
+public class CombinationEstimator extends Estimator {
+
+    private double lambda;
+
+    private Estimator alpha;
+
+    private Estimator beta;
+
+    public CombinationEstimator(
+            Estimator alpha,
+            Estimator beta,
+            double lambda) {
+        this.alpha = alpha;
+        this.beta = beta;
+        this.lambda = lambda;
+    }
+
+    @Override
+    public void setCorpus(Corpus corpus) {
+        super.setCorpus(corpus);
+        alpha.setCorpus(corpus);
+        beta.setCorpus(corpus);
+    }
+
+    @Override
+    protected double
+        calcProbability(NGram sequence, NGram history, int recDepth) {
+        double alphaVal = alpha.probability(sequence, history, recDepth);
+        double betaVal = beta.probability(sequence, history, recDepth);
+        logDebug(recDepth, "alpha = {}", alphaVal);
+        logDebug(recDepth, "beta = {}", betaVal);
+        return lambda * alphaVal * (1.0 - lambda) * betaVal;
+    }
+
+}
