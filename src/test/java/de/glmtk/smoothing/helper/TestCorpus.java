a883e33fdc64a32b5fe51bb6839b8e899cd1bb42
Lukas Schmelzeisen <l.schmelzeisen@gmx.de>
2014-11-24 00:14:29 +0100

    Cleaned up TestCorpus class.

--- src/test/java/de/glmtk/smoothing/helper/TestCorpus.java
+++ src/test/java/de/glmtk/smoothing/helper/TestCorpus.java
@@ -1,9 +1,6 @@
 package de.glmtk.smoothing.helper;
 
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Collections;
 import java.util.LinkedList;
@@ -13,7 +10,6 @@ import java.util.Set;
 import de.glmtk.Constants;
 import de.glmtk.Glmtk;
 import de.glmtk.querying.CountCache;
-import de.glmtk.utils.StringUtils;
 
 public class TestCorpus {
 
@@ -29,13 +25,13 @@ public class TestCorpus {
         }
     }
 
-    private String corpusName = null;
+    private String corpusName;
 
-    private Path corpus = null;
+    private Path corpus;
 
-    private Path workingDir = null;
+    private Path workingDir;
 
-    private CountCache countCache = null;
+    private CountCache countCache;
 
     private TestCorpus(
             String corpusName) throws IOException, InterruptedException {
@@ -56,54 +52,6 @@ public class TestCorpus {
         glmtk.setCorpus(corpus);
         glmtk.setWorkingDir(workingDir);
         glmtk.count();
-
-        //        workingDir = output;
-        //
-        //        if (!Files.exists(output)) {
-        //            Files.createDirectory(output);
-        //        }
-
-        //        Path indexFile = workingDir.resolve("index.txt");
-        //        Path sequencesDir = workingDir.resolve("sequences");
-        //        absoluteDir = workingDir.resolve("absolute");
-        //        continuationDir = workingDir.resolve("continuation");
-        //
-        //        // index
-        //        if (!Files.exists(indexFile)) {
-        //            try (InputStream input = Files.newInputStream(trainingFile);
-        //                    OutputStream output = Files.newOutputStream(indexFile)) {
-        //                IndexBuilder indexBuilder = new IndexBuilder(false, false, 5);
-        //                indexBuilder.buildIndex(input, output, 1, 1);
-        //            }
-        //        }
-        //        try (InputStream input = Files.newInputStream(indexFile)) {
-        //            index = new Index(input);
-        //        }
-        //
-        //        // sequences
-        //        if (!Files.exists(sequencesDir)) {
-        //            Sequencer sequencer =
-        //                    new Sequencer(trainingFile, sequencesDir, index, 1, false,
-        //                            false);
-        //            sequencer.sequence(Pattern.getCombinations(5,
-        //                    Arrays.asList(PatternElem.CNT, PatternElem.SKP)));
-        //        }
-        //
-        //        // absolute
-        //        if (!Files.exists(absoluteDir)) {
-        //            AbsoluteCounter absoluteCounter =
-        //                    new AbsoluteCounter(sequencesDir, absoluteDir, "\t", 1,
-        //                            false, true);
-        //            absoluteCounter.count();
-        //        }
-        //
-        //        // continuation
-        //        if (!Files.exists(continuationDir)) {
-        //            ContinuationCounter continuationCounter =
-        //                    new ContinuationCounter(absoluteDir, continuationDir,
-        //                            index, "\t", 1, false, true);
-        //            continuationCounter.count();
-        //        }
     }
 
     public String getCorpusName() {
@@ -120,8 +68,6 @@ public class TestCorpus {
 
     /**
      * Lazily loaded.
-     * 
-     * @throws IOException
      */
     public CountCache getCountCache() throws IOException {
         if (countCache == null) {
@@ -144,52 +90,5 @@ public class TestCorpus {
         Collections.reverse(result);
         return result;
     }
-
-    // TODO: what is this?
-    @Deprecated
-    public Path getSequencesTestingSample(int length) throws IOException {
-        Path sequencesTestSample =
-                workingDir.resolve("sequences-testing-samples-" + length);
-        if (!Files.exists(sequencesTestSample)) {
-            try (BufferedWriter writer =
-                    Files.newBufferedWriter(sequencesTestSample,
-                            Charset.defaultCharset())) {
-                for (int i = 0; i != ((int) Math.pow(getWords().length, length)); ++i) {
-                    writer.write(StringUtils.join(getSequenceList(i, length),
-                            " "));
-                    writer.write("\n");
-                }
-            }
-        }
-        return sequencesTestSample;
-    }
-
-    //    public Path getWorkingDir() {
-    //        return workingDir;
-    //    }
-    //
-    //    public void setWorkingDir(Path workingDir) {
-    //        this.workingDir = workingDir;
-    //    }
-    //
-    //    public Path getAbsoluteDir() {
-    //        return absoluteDir;
-    //    }
-    //
-    //    public void setAbsoluteDir(Path absoluteDir) {
-    //        this.absoluteDir = absoluteDir;
-    //    }
-    //
-    //    public Path getContinuationDir() {
-    //        return continuationDir;
-    //    }
-    //
-    //    public void setContinuationDir(Path continuationDir) {
-    //        this.continuationDir = continuationDir;
-    //    }
-    //
-    //    public CountCache getCorpus() throws IOException {
-    //        return new CountCache(getAbsoluteDir(), getContinuationDir());
-    //    }
 
 }
