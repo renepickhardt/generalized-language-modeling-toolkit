renepickhardt/generalized-language-modeling-toolkit

commit 46fa7bc5e08c1860b61417b67e9e327c9a25713f

Fixed a bug that would cause multiple Estimators with different CountCaches to produce wrong results. #90

src/main/java/de/glmtk/querying/estimator/Estimator.java
@@ -1,20 +1,20 @@
 /*
  * Generalized Language Modeling Toolkit (GLMTK)
- *
+ * 
  * Copyright (C) 2014-2015 Lukas Schmelzeisen, Rene Pickhardt
- *
+ * 
  * GLMTK is free software: you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
  * version.
- *
+ * 
  * GLMTK is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
  * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License along with
  * GLMTK. If not, see <http://www.gnu.org/licenses/>.
- *
+ * 
  * See the AUTHORS file for contributors.
  */
 
@@ -32,6 +32,7 @@ import org.apache.logging.log4j.Logger;
 import de.glmtk.common.CountCache;
 import de.glmtk.common.NGram;
 import de.glmtk.common.ProbMode;
+import de.glmtk.querying.estimator.substitute.AbsoluteUnigramEstimator;
 import de.glmtk.querying.estimator.substitute.SubstituteEstimator;
 import de.glmtk.util.StringUtils;
 
@@ -62,7 +63,7 @@ public abstract class Estimator {
         LOGGER.trace(StringUtils.repeat("  ", recDepth) + format, params);
     }
 
-    protected final SubstituteEstimator SUBSTITUTE_ESTIMATOR = Estimators.ABS_UNIGRAM;
+    protected final SubstituteEstimator SUBSTITUTE_ESTIMATOR = new AbsoluteUnigramEstimator();
     private String name = "Unnamed";
     protected CountCache countCache = null;
     protected ProbMode probMode = null;

commit d4e7807c77fd6e25967e1375225438aa96be5842

Fixed bug infinite recursion bug introduced in 46fa7bc5e08c1860b61417b67e9e327c9a25713f.

src/main/java/de/glmtk/querying/estimator/Estimator.java
@@ -63,10 +63,20 @@ public abstract class Estimator {
         LOGGER.trace(StringUtils.repeat("  ", recDepth) + format, params);
     }
 
-    protected final SubstituteEstimator SUBSTITUTE_ESTIMATOR = new AbsoluteUnigramEstimator();
-    private String name = "Unnamed";
-    protected CountCache countCache = null;
-    protected ProbMode probMode = null;
+    protected final SubstituteEstimator SUBSTITUTE_ESTIMATOR;
+    private String name;
+    protected CountCache countCache;
+    protected ProbMode probMode;
+
+    public Estimator() {
+        if (this instanceof SubstituteEstimator)
+            SUBSTITUTE_ESTIMATOR = null;
+        else
+            SUBSTITUTE_ESTIMATOR = new AbsoluteUnigramEstimator();
+        name = "Unnamed";
+        countCache = null;
+        probMode = null;
+    }
 
     @Override
     public String toString() {