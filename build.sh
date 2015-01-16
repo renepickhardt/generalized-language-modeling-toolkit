01a5cff5013a6fd96cb8970248d4e053956f4641
Lukas Schmelzeisen <l.schmelzeisen@gmx.de>
2015-01-16 10:54:15 +0100

    Fixed potetential bugs in bash scripts.

--- build.sh
+++ build.sh
@@ -21,9 +21,9 @@
 set -u # trap uses of unset variables
 set -e # exit on unchecked failure
 
-if [[ $1 == "clean" ]]
+if [[ "${1-}" == "clean" ]]
 then
     mvn clean
 else
-    mvn compile assembly:single
+     mvn compile assembly:single
 fi
--- run.sh
+++ run.sh
@@ -24,16 +24,16 @@ set -e # exit on unchecked failure
 # get script location
 GLMTK_DIR=`readlink -f $BASH_SOURCE | xargs dirname`
 
-CONFIG_FILE=$GLMTK_DIR/glmtk.conf
-JAR_FILE=$GLMTK_DIR/target/glmtk-0.0.1-SNAPSHOT-jar-with-dependencies.jar
+CONFIG_FILE="$GLMTK_DIR/glmtk.conf"
+JAR_FILE="$GLMTK_DIR/target/glmtk-0.0.1-SNAPSHOT-jar-with-dependencies.jar"
 
-if [[ ! -f $CONFIG_FILE ]]; then
+if [[ ! -f "$CONFIG_FILE" ]]; then
     echo "Config file missing: Could not open '$CONFIG_FILE'."
-    echo "Dic you copy 'glmtk.config.sample' to 'glmtk.conf' in the installation directory '$GLMTK_DIR'?"
+    echo "Did you copy 'glmtk.config.sample' to 'glmtk.conf' in the installation directory '$GLMTK_DIR'?"
     exit
 fi
 
-if [[ ! -f $JAR_FILE ]]; then
+if [[ ! -f "$JAR_FILE" ]]; then
     echo "Jar file missing: Could not open '$JAR_FILE'."
     echo "Did you execute './build.sh' in the installation directory '$GLMTK_DIR'?"
     exit
@@ -41,7 +41,7 @@ fi
 
 # Calculate jvm memory
 DEFAULT_JVM_MEMORY=4096
-JVM_MEMORY=`grep -oP "^\s+jvm:\s+\K\d+" $CONFIG_FILE`
+JVM_MEMORY=`grep -oP "^\s+jvm:\s+\K\d+" "$CONFIG_FILE"`
 if [[ -z $JVM_MEMORY ]]; then
     JVM_MEMORY=$DEFAULT_JVM_MEMORY
 fi
@@ -56,4 +56,4 @@ fi
 
 # TODO: ulimit unlimited?
 ulimit -v 20000000
-nice java $MAVEN_OPTS -Dglmtk.dir="$GLMTK_DIR" -Dglmtk.isttyStderr="$IS_TTY_STDERR" -Dfile.encoding="UTF-8" -jar $JAR_FILE "$@"
+nice java $MAVEN_OPTS -Dglmtk.dir="$GLMTK_DIR" -Dglmtk.isttyStderr="$IS_TTY_STDERR" -Dfile.encoding="UTF-8" -jar "$JAR_FILE" "$@"
