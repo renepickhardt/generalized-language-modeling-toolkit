dbb0aac9a5117c80a158079a2bfa4321ee5211a8
Lukas Schmelzeisen <l.schmelzeisen@gmx.de>
2015-01-19 11:46:34 +0100

    Expand documentation of StringUtils#splitAtChar.

--- src/main/java/de/glmtk/util/StringUtils.java
+++ src/main/java/de/glmtk/util/StringUtils.java
@@ -1,20 +1,20 @@
 /*
  * Generalized Language Modeling Toolkit (GLMTK)
- * 
+ *
  * Copyright (C) 2014-2015 Lukas Schmelzeisen
- * 
+ *
  * GLMTK is free software: you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
  * version.
- * 
+ *
  * GLMTK is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
  * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with
  * GLMTK. If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * See the AUTHORS file for contributors.
  */
 
@@ -35,11 +35,16 @@ public class StringUtils {
      * Takes a {@code string} and returns a list containing all substrings which
      * are separated by character {@code split}.
      *
+     * <p>
+     * If {@code split} occurs multiple times after another, it is treated as
+     * one.
+     *
+     * <p>
      * This method should be used for space splitting instead of
      * {@code string.split(' ')} since it is much faster.
      */
     public static List<String> splitAtChar(String string,
-                                           char split) {
+            char split) {
         List<String> result = new ArrayList<>();
 
         int sp1 = 0, sp2;
