f0e7023c0dbfaea3564beb03ac0087ca8ddee35b
Lukas Schmelzeisen <l.schmelzeisen@gmx.de>
2015-01-19 13:37:03 +0100

    Add LambdaCounts abstraction class plus Reader/Writer.

--- src/main/java/de/glmtk/files/LambdaCountsReader.java
+++ src/main/java/de/glmtk/files/LambdaCountsReader.java
@@ -1,20 +1,20 @@
 /*
  * Generalized Language Modeling Toolkit (GLMTK)
- * 
+ *
  * Copyright (C) 2015 Lukas Schmelzeisen
- * 
+ *
  * GLMTK is free software: you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
  * version.
- * 
+ *
  * GLMTK is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
  * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with
  * GLMTK. If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * See the AUTHORS file for contributors.
  */
 
@@ -25,46 +25,47 @@ import java.nio.charset.Charset;
 import java.nio.file.Path;
 import java.util.List;
 
-import de.glmtk.counts.AlphaCount;
+import de.glmtk.counts.LambdaCounts;
 import de.glmtk.exceptions.FileFormatException;
 import de.glmtk.util.StringUtils;
 
-public class AlphaCountReader extends AbstractFileReader {
+public class LambdaCountsReader extends AbstractFileReader {
     private String sequence;
-    private AlphaCount alphaCount;
+    private LambdaCounts lambdaCounts;
 
-    public AlphaCountReader(Path file,
-                            Charset charset) throws IOException {
+    public LambdaCountsReader(Path file,
+                              Charset charset) throws IOException {
         this(file, charset, 8192);
     }
 
-    public AlphaCountReader(Path file,
-                            Charset charset,
-                            int sz) throws IOException {
+    public LambdaCountsReader(Path file,
+                              Charset charset,
+                              int sz) throws IOException {
         super(file, charset, sz);
         sequence = null;
-        alphaCount = null;
+        lambdaCounts = null;
     }
 
     @Override
     protected void parseLine() {
         if (line == null) {
             sequence = null;
-            alphaCount = null;
+            lambdaCounts = null;
             return;
         }
 
         try {
             List<String> split = StringUtils.splitAtChar(line, '\t');
-            if (split.size() != 3)
+            if (split.size() < 2)
                 throw new IllegalArgumentException(
-                        "Expected line to have format '<sequence>\\t<normal>\\t<discounted>'.");
+                        "Expected line to have format '<sequence>\\t<lambda>+'.");
 
             sequence = split.get(0);
-            alphaCount = new AlphaCount(parseFloatingPoint(split.get(1)),
-                    parseFloatingPoint(split.get(2)));
+            lambdaCounts = new LambdaCounts();
+            for (int i = 1; i != split.size(); ++i)
+                lambdaCounts.add(parseFloatingPoint(split.get(i)));
         } catch (IllegalArgumentException e) {
-            throw new FileFormatException(line, lineNo, file, "alpha counts",
+            throw new FileFormatException(line, lineNo, file, "lambda counts",
                     e.getMessage());
         }
     }
@@ -73,7 +74,7 @@ public class AlphaCountReader extends AbstractFileReader {
         return sequence;
     }
 
-    public AlphaCount getAlphaCounts() {
-        return alphaCount;
+    public LambdaCounts getLambdaCounts() {
+        return lambdaCounts;
     }
 }
