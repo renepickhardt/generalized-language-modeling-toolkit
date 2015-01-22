59e9b527ecda91f11f91200caf68d515c2d76076
Lukas Schmelzeisen <l.schmelzeisen@gmx.de>
2015-01-22 17:42:55 +0100

    Make test execute faster, by not querying with estimators that already ran in previous test cases.

--- src/test/java/de/glmtk/querying/OptimizedEstimatorTest.java
+++ src/test/java/de/glmtk/querying/OptimizedEstimatorTest.java
@@ -1,20 +1,20 @@
 /*
  * Generalized Language Modeling Toolkit (GLMTK)
- * 
+ *
  * Copyright (C) 2015 Lukas Schmelzeisen
- * 
+ *
  * GLMTK is free software: you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
  * version.
- * 
+ *
  * GLMTK is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
  * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with
  * GLMTK. If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * See the AUTHORS file for contributors.
  */
 
@@ -82,8 +82,10 @@ public class OptimizedEstimatorTest extends TestCorporaTest {
         this.optimized = optimized;
         this.slow = slow;
 
-        hashes.put(optimized, getHashForEstimatedTestFile(optimized));
-        hashes.put(slow, getHashForEstimatedTestFile(slow));
+        if (!hashes.containsKey(optimized))
+            hashes.put(optimized, getHashForEstimatedTestFile(optimized));
+        if (!hashes.containsKey(slow))
+            hashes.put(slow, getHashForEstimatedTestFile(slow));
     }
 
     private String getHashForEstimatedTestFile(Estimator estimator) throws Exception {
