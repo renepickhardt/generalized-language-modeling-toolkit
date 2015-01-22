b510a9db7289744245223d44049ac158e1a4337b
Lukas Schmelzeisen <l.schmelzeisen@gmx.de>
2015-01-22 14:33:25 +0100

    Create OptimizedEstimatorTest from FastEstimatorTest.

--- src/test/java/de/glmtk/querying/OptimizedEstimatorTest.java
+++ src/test/java/de/glmtk/querying/OptimizedEstimatorTest.java
@@ -21,6 +21,7 @@
 package de.glmtk.querying;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assume.assumeNotNull;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -42,66 +43,98 @@ import de.glmtk.common.Pattern;
 import de.glmtk.logging.Logger;
 import de.glmtk.querying.estimator.Estimator;
 import de.glmtk.querying.estimator.Estimators;
+import de.glmtk.querying.estimator.fast.FastModifiedKneserNeyAbsEstimator;
 import de.glmtk.querying.estimator.fast.FastModifiedKneserNeyEstimator;
 import de.glmtk.querying.estimator.learned.LearnedModKneserNeyEstimator;
 import de.glmtk.testutil.TestCorporaTest;
 import de.glmtk.testutil.TestCorpus;
 import de.glmtk.util.HashUtils;
 
+/**
+ * Test optimized estimator implementation using the slower ones.
+ */
 @RunWith(Parameterized.class)
-public class FastEstimatorTest extends TestCorporaTest {
-    private static final Logger LOGGER = Logger.get(FastEstimatorTest.class);
+public class OptimizedEstimatorTest extends TestCorporaTest {
+    private static final Logger LOGGER = Logger.get(OptimizedEstimatorTest.class);
 
     @Parameters(name = "{0}")
     public static Iterable<Object[]> data() {
+        Estimator slowMknAbs = Estimators.MOD_KNESER_NEY_ABS;
+        Estimator fastMknAbs = new FastModifiedKneserNeyAbsEstimator();
+        fastMknAbs.setName("Fast-Modified-Kneser-Ney (Abs-Lower-Order)");
+        Estimator learnedMknAbs = null;
+
         Estimator slowMkn = Estimators.MOD_KNESER_NEY;
         Estimator fastMkn = new FastModifiedKneserNeyEstimator();
         fastMkn.setName("Fast-Modified-Kneser-Ney");
         Estimator learnedMkn = new LearnedModKneserNeyEstimator();
         learnedMkn.setName("Learned-Modified-Kneser-Ney");
 
-        return Arrays.asList(new Object[][] {{"ModKneserNey", slowMkn, fastMkn,
-                learnedMkn}});
+        Estimator slowGlmAbs = Estimators.GLM_ABS;
+        Estimator fastGlmAbs = null;
+        Estimator learnedGlmAbs = null;
+
+        Estimator slowGlm = Estimators.GLM;
+        Estimator fastGlm = null;
+        Estimator learnedGlm = null;
+
+        return Arrays.asList(new Object[][] {
+                //@formatter:off
+                {"ModKneserNey",        slowMkn,    fastMkn,    learnedMkn},
+                {"ModKneserNey (Abs)",  slowMknAbs, fastMknAbs, learnedMknAbs},
+                {"GeneralizedLM",       slowGlm,    fastGlm,    learnedGlm},
+                {"GeneralizedLM (Abs)", slowGlmAbs, fastGlmAbs, learnedGlmAbs}
+                //@formatter:on
+        });
     }
 
     private static TestCorpus testCorpus = TestCorpus.EN0008T;
     private static Path testFile = Constants.TEST_RESSOURCES_DIR.resolve("en0008t.testing.5");
 
+    /*
+     * We have to use static data for what would otherwise be put into member
+     * vaiables, because Junit ParameterizedRunner will instantiate the class
+     * for every test method newly.
+     */
     private static Set<String> initliazed = new HashSet<>();
     private static Map<String, String> hashSlow = new HashMap<>();
     private static Map<String, String> hashFast = new HashMap<>();
     private static Map<String, String> hashLearned = new HashMap<>();
 
     private String name;
+    private Estimator fastEstimator;
+    private Estimator learnedEstimator;
 
-    public FastEstimatorTest(String name,
-                             Estimator slowEstimator,
-                             Estimator fastEstimator,
-                             Estimator learnedEstimator) throws Exception {
+    /**
+     * @param fastEstimator
+     *            May be {@code null}.
+     * @param learnedEstimator
+     *            May be {@code null}.
+     */
+    public OptimizedEstimatorTest(String name,
+                                  Estimator slowEstimator,
+                                  Estimator fastEstimator,
+                                  Estimator learnedEstimator) throws Exception {
         this.name = name;
+        this.fastEstimator = fastEstimator;
+        this.learnedEstimator = learnedEstimator;
 
         if (initliazed.contains(name))
             return;
 
-        Set<Pattern> patternsSlow = slowEstimator.getUsedPatterns(5);
-        Set<Pattern> patternsFast = fastEstimator.getUsedPatterns(5);
-        Set<Pattern> patternsLearned = learnedEstimator.getUsedPatterns(5);
-
-        Set<Pattern> patterns = new HashSet<>();
-        patterns.addAll(patternsSlow);
-        patterns.addAll(patternsFast);
-        patterns.addAll(patternsLearned);
-
-        LOGGER.debug("patternsSlow    : %s", patternsSlow);
-        LOGGER.debug("patternsFast    : %s", patternsFast);
-        LOGGER.debug("patternsLearned : %s", patternsLearned);
-        LOGGER.debug("patterns        : %s", patterns);
+        Set<Pattern> patterns = slowEstimator.getUsedPatterns(5);
+        if (fastEstimator != null)
+            patterns.addAll(fastEstimator.getUsedPatterns(5));
+        if (learnedEstimator != null)
+            patterns.addAll(learnedEstimator.getUsedPatterns(5));
 
         Cache cache = testCorpus.getCache(patterns);
 
         hashSlow.put(name, runEstimatorOnFile(slowEstimator, cache));
-        hashFast.put(name, runEstimatorOnFile(fastEstimator, cache));
-        hashLearned.put(name, runEstimatorOnFile(learnedEstimator, cache));
+        if (fastEstimator != null)
+            hashFast.put(name, runEstimatorOnFile(fastEstimator, cache));
+        if (learnedEstimator != null)
+            hashLearned.put(name, runEstimatorOnFile(learnedEstimator, cache));
 
         initliazed.add(name);
     }
@@ -128,11 +161,13 @@ public class FastEstimatorTest extends TestCorporaTest {
 
     @Test
     public void testFast() {
+        assumeNotNull(fastEstimator);
         assertEquals(hashSlow.get(name), hashFast.get(name));
     }
 
     @Test
     public void testLearned() {
+        assumeNotNull(learnedEstimator);
         assertEquals(hashSlow.get(name), hashLearned.get(name));
     }
 }
