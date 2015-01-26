9bcac424120855d12b39415d60bdef59f53dceb6
Lukas Schmelzeisen <l.schmelzeisen@gmx.de>
2015-01-26 17:53:38 +0100

    Remove abstract part of AbstractSequenceReader.

--- src/main/java/de/glmtk/files/SequenceReader.java
+++ src/main/java/de/glmtk/files/SequenceReader.java
@@ -1,20 +1,20 @@
 /*
  * Generalized Language Modeling Toolkit (GLMTK)
- * 
+ *
  * Copyright (C) 2014-2015 Lukas Schmelzeisen
- * 
+ *
  * GLMTK is free software: you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
  * version.
- * 
+ *
  * GLMTK is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
  * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with
  * GLMTK. If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * See the AUTHORS file for contributors.
  */
 
@@ -27,8 +27,8 @@ import java.util.Comparator;
 
 import de.glmtk.util.ObjectUtils;
 
-public abstract class AbstractSequenceReader extends AbstractFileReader {
-    public static class SequenceComparator<T extends AbstractSequenceReader> implements Comparator<T> {
+public class SequenceReader extends AbstractFileReader {
+    public static class SequenceComparator<T extends SequenceReader> implements Comparator<T> {
         @Override
         public int compare(T lhs,
                            T rhs) {
@@ -44,15 +44,15 @@ public abstract class AbstractSequenceReader extends AbstractFileReader {
 
     protected String sequence;
 
-    public AbstractSequenceReader(Path file,
-                                  Charset charset) throws IOException {
+    public SequenceReader(Path file,
+                          Charset charset) throws IOException {
         this(file, charset, 8192);
         sequence = null;
     }
 
-    public AbstractSequenceReader(Path file,
-                                  Charset charset,
-                                  int sz) throws IOException {
+    public SequenceReader(Path file,
+                          Charset charset,
+                          int sz) throws IOException {
         super(file, charset, sz);
     }
 
