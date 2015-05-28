dc6ecbac5846043d8e2159527d4e1a9d8b6aa2fb
Lukas Schmelzeisen <l.schmelzeisen@gmx.de>
2015-05-28 10:11:05 +0200

    Add option to control buffering of log files (now disabled by default). This causes log messages to now appear immediatly in log files.

--- src/main/java/de/glmtk/logging/Log4jHelper.java
+++ src/main/java/de/glmtk/logging/Log4jHelper.java
@@ -1,20 +1,20 @@
 /*
  * Generalized Language Modeling Toolkit (GLMTK)
- *
+ * 
  * Copyright (C) 2014-2015 Lukas Schmelzeisen
- *
+ * 
  * GLMTK is free software: you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
  * version.
- *
+ * 
  * GLMTK is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
  * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License along with
  * GLMTK. If not, see <http://www.gnu.org/licenses/>.
- *
+ * 
  * See the AUTHORS file for contributors.
  */
 
@@ -46,31 +46,38 @@ public class Log4jHelper {
     private static final String LOGGING_PATTERN = "%date{yyyy-MM-dd HH:mm:ss} [%-5level]  %-100msg  [%class{1}#%method:%line] %thread (%logger)%n";
 
     /**
-     * Log4j LoggerContext.
+     * Buffer size to use for file appenders.
+     *
+     * Note that buffering has to be enabled in
+     * {@link #initLoggingHelper(boolean)} for this to have any effect.
      */
+    private static final int BUFFER_SIZE = 8192;
+
+    /** Log4j LoggerContext. */
     private static LoggerContext context;
-
-    /**
-     * Log4j Configuration.
-     */
+    /** Log4j Configuration. */
     private static Configuration config;
-
-    /**
-     * Serializable Log4j Root Logger Configuration.
-     */
+    /** Serializable Log4j Root Logger Configuration. */
     private static LoggerConfig rootLoggerConfig;
-
     private static Layout<String> layout;
-
+    private static boolean useBuffer;
     private static boolean initialized = false;
 
-    public static void initLoggingHelper() {
+    /**
+     * @param bufferLogging
+     *            Whether output to logfile should be buffered. Enabling
+     *            buffering increases performance, as less locks will occur.
+     *            However one will not see log messages directly in the log
+     *            file.
+     */
+    public static void initLoggingHelper(boolean bufferLogging) {
         context = (LoggerContext) LogManager.getContext(false);
         config = context.getConfiguration();
         rootLoggerConfig = config.getLoggerConfig(LogManager.ROOT_LOGGER_NAME);
         layout = PatternLayout.createLayout(LOGGING_PATTERN, config, null,
                 Constants.CHARSET, true, true, null, null);
 
+        useBuffer = bufferLogging;
         initialized = true;
     }
 
@@ -119,17 +126,20 @@ public class Log4jHelper {
      *            {@code false} log message replace file contents.
      */
     public static void addLoggingFileAppender(Path logFile,
-                                       String name,
-                                       boolean append) {
+                                              String name,
+                                              boolean append) {
         checkInitialized();
         checkNotNull(logFile);
         checkNotNull(name);
         checkArgument(!name.isEmpty());
 
+        int bufferSize = useBuffer ? BUFFER_SIZE : 0;
+
         Appender fileLocalAppender = FileAppender.createAppender(
                 logFile.toString(), Boolean.toString(append), "false", name,
-                "false", "false", "true", "8192", layout, null, "false",
-                "false", config);
+                "false", "false", Boolean.toString(useBuffer),
+                Integer.toString(bufferSize), layout, null, "false", "false",
+                config);
         fileLocalAppender.start();
         config.addAppender(fileLocalAppender);
         rootLoggerConfig.addAppender(fileLocalAppender, null, null);
