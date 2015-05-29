51ffa4eac23198508be5b92fb3c0c4e9f555f8f6
Lukas Schmelzeisen <l.schmelzeisen@gmx.de>
2015-05-29 09:58:32 +0200

    Add newBufferedWriter helper to Files class.

--- src/main/java/de/glmtk/util/Files.java
+++ src/main/java/de/glmtk/util/Files.java
@@ -1,13 +1,21 @@
 package de.glmtk.util;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.nio.charset.Charset;
 
 public class Files {
     public static BufferedReader newBufferedReader(InputStream inputstream,
                                                    Charset charset) {
         return new BufferedReader(new InputStreamReader(inputstream, charset));
+    }
+
+    public static BufferedWriter newBufferedWriter(OutputStream outputstream,
+                                                   Charset charset) {
+        return new BufferedWriter(new OutputStreamWriter(outputstream, charset));
     }
 }
