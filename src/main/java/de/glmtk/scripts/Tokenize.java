9927bb75ce86997c1b278422e8d4a826e7a63144
Lukas Schmelzeisen <l.schmelzeisen@gmx.de>
2015-07-06 22:54:10 +0200

    Made script to tokenize corpus texts.

---
+++ src/main/java/de/glmtk/scripts/Tokenize.java
@@ -0,0 +1,76 @@
+package de.glmtk.scripts;
+
+import static java.lang.Character.charCount;
+import static java.lang.Character.isLetterOrDigit;
+import static java.lang.Character.toChars;
+import static java.nio.file.Files.newBufferedReader;
+import static java.nio.file.Files.newBufferedWriter;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import de.glmtk.Constants;
+
+/**
+ * Takes a file and puts spaces between all tokens.
+ *
+ * Example: {@code She said: "Hello you're 13th".} becomes
+ * {@code She said : " Hello you ' re 13th " .}
+ */
+public class Tokenize {
+    public static void main(String[] args) throws IOException {
+        // Path to filter for vocab.
+        Path inputFile = Paths.get("/home/lukas/langmodels/data/oanc.untokenized");
+        // Path to write filtered output to.
+        Path outputFile = Paths.get("/home/lukas/langmodels/data/oanc");
+
+        //        String input = "She said: \"Hello you're 13th\".";
+        //        System.out.println(tokenizeLine(input));
+        try (BufferedReader reader = newBufferedReader(inputFile,
+                Constants.CHARSET);
+                BufferedWriter writer = newBufferedWriter(outputFile,
+                        Constants.CHARSET)) {
+            String line;
+            while ((line = reader.readLine()) != null)
+                writer.append(tokenizeLine(line)).append('\n');
+        }
+    }
+
+    public static String tokenizeLine(String line) {
+        StringBuilder result = new StringBuilder();
+
+        int offset = 0;
+        int length = line.length();
+        boolean lastCharWasSpace = true;
+        while (offset != length) {
+            int curChar = line.codePointAt(offset);
+            offset += charCount(curChar);
+
+            if (isLetterOrDigit(curChar)) {
+                result.append(toChars(curChar));
+                lastCharWasSpace = false;
+            } else if (curChar == ' ') {
+                if (!lastCharWasSpace)
+                    result.append(' ');
+                lastCharWasSpace = true;
+            } else { // curChar is special character
+                if (!lastCharWasSpace)
+                    result.append(' ');
+                result.append(toChars(curChar));
+                result.append(' ');
+                lastCharWasSpace = true;
+            }
+        }
+
+        // Delete trailing whitespace that occurs if last char was a special
+        // character
+        int resultLength = result.length();
+        if (resultLength != 0 && result.charAt(resultLength - 1) == ' ')
+            result.deleteCharAt(resultLength - 1);
+
+        return result.toString();
+    }
+}
