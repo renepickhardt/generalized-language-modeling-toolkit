07188be8846857f0caab974632850fe056817d3f
Lukas Schmelzeisen <l.schmelzeisen@gmx.de>
2015-07-18 16:01:38 +0200

    Make install script skip already installed targets.

--- install.sh
+++ install.sh
@@ -4,10 +4,16 @@ set -u # trap uses of unset variables
 set -e # exit on unchecked failure
 
 path=`pwd`
+install_dir="/usr/local/bin/"
 
 for script in `find . -maxdepth 1 -executable -type f -name "glmtk*"`
 do
 	script=${script:2}
-	echo "installing ${script}..."
-	sudo ln -s "${path}/${script}" "/usr/local/bin/${script}"
+    if [ -h "${install_dir}/${script}" ]
+    then
+        echo "Found ${script} installed, skipping..."
+    else
+	    echo "installing ${script}..."
+	    sudo ln -s "${path}/${script}" "${install_dir}/${script}"
+    fi
 done
